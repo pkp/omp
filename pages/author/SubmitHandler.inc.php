5db6ceff4d37247ff26dabc9564fec9309cd77fa
jalperin <jalperin>
2009-05-25 20:43:27 +0000

    #4333# synced with OJS and took out commented code

--- pages/author/SubmitHandler.inc.php
+++ pages/author/SubmitHandler.inc.php
@@ -17,6 +17,12 @@
 import('pages.author.AuthorHandler');
 
 class SubmitHandler extends AuthorHandler {
+	/** monograph associated with the request **/
+	var $monograph;
+
+	/**
+	 * Constructor
+	 **/
 	function SubmitHandler() {
 		parent::AuthorHandler();
 	}
@@ -27,11 +33,12 @@ class SubmitHandler extends AuthorHandler {
 	 * @param $args array optional, if set the first parameter is the step to display
 	 */
 	function submit($args) {
-		$this->validate('author.submit.authorSubmitLoginMessage');
-		$this->setupTemplate(true);
-
 		$step = isset($args[0]) ? $args[0] : 0;
 		$monographId = Request::getUserVar('monographId');
+		$this->validate($monographId, 'author.submit.authorSubmitLoginMessage');
+
+		$monograph =& $this->monograph;
+		$this->setupTemplate(true);
 		
 		import('author.form.submit.AuthorSubmissionSequence');
 
@@ -51,11 +58,12 @@ class SubmitHandler extends AuthorHandler {
 	 * @param $args array first parameter is the step being saved
 	 */
 	function saveSubmit($args) {
-		$this->validate();
-		$this->setupTemplate(true);
-
 		$step = isset($args[0]) ? $args[0] : 0;
 		$monographId = Request::getUserVar('monographId');
+
+		$this->validate($monographId);
+		$this->setupTemplate(true);
+		$monograph =& $this->monograph;
 
 		import('author.form.submit.AuthorSubmissionSequence');
 		$sequence = new AuthorSubmissionSequence($monographId);
@@ -94,9 +102,9 @@ class SubmitHandler extends AuthorHandler {
 				$templateMgr->assign_by_ref('press', $press);
 				// If this is an editor and there is a
 				// submission file, monograph can be expedited.
-		//		if (Validation::isEditor($press->getId()) && $monograph->getSubmissionFileId()) {
-		//			$templateMgr->assign('canExpedite', true);
-		//		}
+				if (Validation::isEditor($press->getId()) && $monograph->getSubmissionFileId()) {
+					$templateMgr->assign('canExpedite', true);
+				}
 				$templateMgr->assign('monographId', $monographId);
 				$templateMgr->assign('helpTopicId','submission.index');
 				$templateMgr->display('author/submit/complete.tpl');
@@ -115,13 +123,11 @@ class SubmitHandler extends AuthorHandler {
 	 * Create new supplementary file with a uploaded file.
 	 */
 	function submitUploadSuppFile() {
-		$this->validate();
-		$this->setupTemplate(true);
-		$monographDao =& DAORegistry::getDAO('MonographDAO');
 		$monographId = Request::getUserVar('monographId');
 
-		list($press, $monograph) = SubmitHandler::validate($monographId, 5);
-		$monograph =& $monographDao->getMonograph((int) $monographId);
+		$this->validate($monographId, 4);
+		$monograph =& $this->monograph;
+		$this->setupTemplate(true);
 
 		import('author.form.submit.AuthorSubmitSuppFileForm');
 		$submitForm =& new AuthorSubmitSuppFileForm($monograph);
@@ -136,15 +142,12 @@ class SubmitHandler extends AuthorHandler {
 	 * @param $args array optional, if set the first parameter is the supplementary file to edit
 	 */
 	function submitSuppFile($args) {
-		$this->validate();
-		$this->setupTemplate(true);
-		$monographDao =& DAORegistry::getDAO('MonographDAO');
-
 		$monographId = Request::getUserVar('monographId');
 		$suppFileId = isset($args[0]) ? (int) $args[0] : 0;
 
-		list($press, $monograph) = SubmitHandler::validate($monographId, 5);
-		$monograph =& $monographDao->getMonograph((int) $monographId);
+		$this->validate($monographId);
+		$monograph =& $this->monograph;
+		$this->setupTemplate(true);
 
 		import('author.form.submit.AuthorSubmitSuppFileForm');
 		$submitForm =& new AuthorSubmitSuppFileForm($monograph, $suppFileId);
@@ -162,15 +165,12 @@ class SubmitHandler extends AuthorHandler {
 	 * @param $args array optional, if set the first parameter is the supplementary file to update
 	 */
 	function saveSubmitSuppFile($args) {
-		$this->validate();
-		$this->setupTemplate(true);
-		$monographDao =& DAORegistry::getDAO('MonographDAO');
-
 		$monographId = Request::getUserVar('monographId');
 		$suppFileId = isset($args[0]) ? (int) $args[0] : 0;
 
-		list($press, $monograph) = SubmitHandler::validate($monographId, 5);
-		$monograph =& $monographDao->getMonograph((int) $monographId);
+		$this->validate($monographId);
+		$monograph =& $this->monograph;
+		$this->setupTemplate(true);
 
 		import('author.form.submit.AuthorSubmitSuppFileForm');
 		$submitForm =& new AuthorSubmitSuppFileForm($monograph, $suppFileId);
@@ -198,9 +198,9 @@ class SubmitHandler extends AuthorHandler {
 		$monographId = Request::getUserVar('monographId');
 		$suppFileId = isset($args[0]) ? (int) $args[0] : 0;
 
-
-		list($press, $monograph) = SubmitHandler::validate($monographId, 4);
-		$monograph =& $monographDao->getMonograph((int) $monographId);
+		$this->validate($monographId, 4);
+		$monograph =& $this->monograph;
+		$this->setupTemplate(true);
 
 		$suppFileDao =& DAORegistry::getDAO('SuppFileDAO');
 		$suppFile = $suppFileDao->getSuppFile($suppFileId, $monographId);
@@ -216,7 +216,9 @@ class SubmitHandler extends AuthorHandler {
 
 	function expediteSubmission() {
 		$monographId = (int) Request::getUserVar('monographId');
-		list($press, $monograph) = SubmitHandler::validate($monographId);
+		$this->validate($monographId);
+		$press =& Request::getPress();
+		$monograph =& $this->monograph;
 
 		// The author must also be an editor to perform this task.
 		if (Validation::isEditor($press->getId()) && $monograph->getSubmissionFileId()) {
@@ -226,6 +228,31 @@ class SubmitHandler extends AuthorHandler {
 		}
 
 		Request::redirect(null, null, 'track');
+	}
+
+	/**
+	 * Validation check for submission.
+	 * Checks that monograph ID is valid, if specified.
+	 * @param $monographId int
+	 */
+	function validate($monographId = null, $reason = null) {
+		parent::validate($reason);
+		$monographDao =& DAORegistry::getDAO('MonographDAO');
+		$user =& Request::getUser();
+		$press =& Request::getPress();
+
+		$monograph = null;
+
+		// Check that monograph exists for this press and user and that submission is incomplete
+		if (isset($monographId)) {
+			$monograph =& $monographDao->getMonograph((int) $monographId);
+			if (!$monograph || $monograph->getUserId() !== $user->getId() || $monograph->getPressId() !== $press->getId()) {
+				Request::redirect(null, null, 'submit');
+			}
+		}
+
+		$this->monograph =& $monograph;
+		return true;
 	}
 }
 ?>
