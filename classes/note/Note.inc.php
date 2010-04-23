5adb7c4f5d758208c766af37139d915dd5fad6f6
Matthew Crider <mattcrider@gmail.com>
2010-04-23 11:50:08 -0700

    *5361* Initial development of Notes infrastructure

---
+++ classes/note/Note.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file classes/note/Note.inc.php
+ *
+ * Copyright (c) 2003-2010 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class Note
+ * @ingroup note
+ * @see NoteDAO
+ * @brief OMP subclass for Notes (defines OMP-specific types)
+ */
+
+// $Id$
+
+/** Notification associative types. */
+define('NOTE_TYPE_EDITOR_TO_REVIEWER', 	0x1000001);
+define('NOTE_TYPE_SUBMISSION_ACCEPTED', 	0x1000002);
+define('NOTE_TYPE_SUBMISSION_DECLINED', 	0x1000003);
+define('NOTE_TYPE_COPYEDITING_FILE', 	0x1000004);
+
+import('note.PKPNote');
+
+class Note extends PKPNote {
+
+	/**
+	 * Constructor.
+	 */
+	function Note() {
+		parent::PKPNote();
+	}
+
+
+ }
+
+?>
