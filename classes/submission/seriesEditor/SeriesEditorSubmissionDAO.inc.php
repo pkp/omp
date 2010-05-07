096f9d3f2bc18add2b46ca3f6c25358b2f374775
Alec Smecher <alec@smecher.bc.ca>
2010-05-07 15:48:26 -0700

    *5386* Review assignment tune-ups

--- classes/submission/seriesEditor/SeriesEditorSubmissionDAO.inc.php
+++ classes/submission/seriesEditor/SeriesEditorSubmissionDAO.inc.php
@@ -1031,7 +1031,7 @@ class SeriesEditorSubmissionDAO extends DAO {
 		);
 		while (!$result->EOF) {
 			$row = $result->GetRowAssoc(false);
-			if (!isset($statistics[$row['reviewer_id']])) $statistics[$row['editor_id']] = array();
+			if (!isset($statistics[$row['reviewer_id']])) $statistics[$row['reviewer_id']] = array();
 			$statistics[$row['reviewer_id']]['last_notified'] = $this->datetimeFromDB($row['last_notified']);
 			$result->MoveNext();
 		}
