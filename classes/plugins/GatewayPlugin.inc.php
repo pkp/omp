602e97377a052358e765028a2de90390d1e1177d
Alec Smecher <alec@smecher.bc.ca>
2010-08-09 11:11:29 -0700

    *4965* Gateway request refactor

--- classes/plugins/GatewayPlugin.inc.php
+++ classes/plugins/GatewayPlugin.inc.php
@@ -109,10 +109,13 @@ class GatewayPlugin extends Plugin {
 
 	/**
 	 * Handle fetch requests for this plugin.
+	 * @param $args array
+	 * @param $request object
 	 */
-	function fetch($args) {
+	function fetch($args, $request) {
 		// Subclasses should override this function.
 		return false;
 	}
 }
+
 ?>
