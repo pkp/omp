6f5df685fb6fe6f1c44463f362f089d094449323
jerico.dev <jerico.dev@gmail.com>
2010-09-20 19:50:38 -0300

    *5887* Create metadata adapters for OMP - monograph adapter

---
+++ plugins/metadata/mods/filter/ModsSchemaMonographAdapter.inc.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file plugins/metadata/mods/filter/ModsSchemaMonographAdapter.inc.php
+ *
+ * Copyright (c) 2000-2010 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ModsSchemaMonographAdapter
+ * @ingroup plugins_metadata_mods_filter
+ * @see Monograph
+ * @see ModsSchema
+ *
+ * @brief Class that inject/extract MODS schema compliant meta-data
+ *  into/from a Monograph object.
+ */
+
+import('lib.pkp.plugins.metadata.filter.ModsSchemaSubmissionAdapter');
+
+class ModsSchemaMonographAdapter extends ModsSchemaSubmissionAdapter {
+	//
+	// Implement template methods from MetadataDataObjectAdapter
+	//
+	/**
+	 * @see MetadataDataObjectAdapter::injectMetadataIntoDataObject()
+	 * @param $modsDescription MetadataDescription
+	 * @param $monograph Monograph
+	 * @param $replace boolean whether to replace the existing monograph
+	 */
+	function &injectMetadataIntoDataObject(&$modsDescription, &$monograph, $replace) {
+		assert(is_a($monograph, 'Monograph'));
+		$monograph =& parent::injectMetadataIntoDataObject($modsDescription, $monograph, $replace, 'classes.monograph.Author');
+
+		// Publication date
+		$publicationDate = $modsDescription->getStatement('originInfo/dateIssued[@keyDate="yes" @encoding="w3cdtf"]');
+		if ($publicationDate) {
+			$monograph->setDatePublished($publicationDate);
+		}
+
+		// ...
+		// FIXME: go through MODS schema and see what context-specific
+		// information needs to be added, e.g. from Press, press settings
+		// or site settings.
+
+		return $monograph;
+	}
+
+	/**
+	 * @see MetadataDataObjectAdapter::extractMetadataFromDataObject()
+	 * @param $monograph Monograph
+	 */
+	function &extractMetadataFromDataObject(&$monograph) {
+		assert(is_a($monograph, 'Monograph'));
+
+		// Define the role of the author(s) of the monograph object
+		// depending on the work type.
+		if ($monograph->getWorkType() == WORK_TYPE_EDITED_VOLUME) {
+			// Marcrelator editor role
+			$authorMarcrelatorRole = 'edt';
+		} else {
+			// Marcrelator author role
+			$authorMarcrelatorRole = 'aut';
+		}
+		$modsDescription =& parent::extractMetadataFromDataObject($monograph, $authorMarcrelatorRole);
+
+		// Publication date
+		$publicationDate = $monograph->getDatePublished();
+		if ($publicationDate) {
+			$modsDescription->addStatement('originInfo/dateIssued[@keyDate="yes" @encoding="w3cdtf"]', $publicationDate);
+		}
+
+		// ...
+		// FIXME: go through MODS schema and see what context-specific
+		// information needs to be added, e.g. from Press, press settings
+		// or site settings.
+
+		return $modsDescription;
+	}
+}
+?>
