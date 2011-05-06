40c43c5f4f449549c81525535d6bf22c98fff02c
Alec Smecher <alec@smecher.bc.ca>
2011-05-06 09:37:58 -0700

    *6125* File grid refactors

--- controllers/grid/files/review/ReviewRevisionsGridHandler.inc.php
+++ controllers/grid/files/review/ReviewRevisionsGridHandler.inc.php
@@ -12,23 +12,24 @@
  * @brief Display the file revisions authors have uploaded
  */
 
-import('controllers.grid.files.fileSignoff.FileSignoffGridHandler');
+import('controllers.grid.files.fileList.FileListGridHandler');
 
-class ReviewRevisionsGridHandler extends FileSignoffGridHandler {
+class ReviewRevisionsGridHandler extends FileListGridHandler {
 	/**
 	 * Constructor
 	 */
 	function ReviewRevisionsGridHandler() {
 		import('controllers.grid.files.review.ReviewRevisionsGridDataProvider');
-		parent::FileSignoffGridHandler(
-			new ReviewRevisionsGridDataProvider(),
+		$dataProvider = new ReviewRevisionsGridDataProvider();
+		parent::FileListGridHandler(
+			$dataProvider,
 			WORKFLOW_STAGE_ID_INTERNAL_REVIEW,
 			FILE_GRID_ADD|FILE_GRID_DOWNLOAD_ALL
 		);
 
 		$this->addRoleAssignment(
 			array(ROLE_ID_SERIES_EDITOR, ROLE_ID_PRESS_MANAGER),
-			array('fetchGrid', 'fetchRow', 'downloadAllFiles', 'signOffFiles')
+			array('fetchGrid', 'fetchRow', 'downloadAllFiles')
 		);
 
 		// Set the grid title.
--- controllers/grid/files/review/SelectableReviewRevisionsGridHandler.inc.php
+++ controllers/grid/files/review/SelectableReviewRevisionsGridHandler.inc.php
@@ -13,120 +13,28 @@
  *   Used for selecting files to send to external review or copyediting.
  */
 
-// import submission files grid specific classes
-import('controllers.grid.files.review.ReviewRevisionsGridHandler');
+import('controllers.grid.files.fileList.SelectableFileListGridHandler');
 
-class SelectableReviewRevisionsGridHandler extends ReviewRevisionsGridHandler {
+class SelectableReviewRevisionsGridHandler extends SelectableFileListGridHandler {
 	/**
 	 * Constructor
 	 */
 	function SelectableReviewRevisionsGridHandler() {
-		parent::ReviewRevisionsGridHandler(false, true);
-	}
+		import('controllers.grid.files.review.ReviewRevisionsGridDataProvider');
+		$dataProvider = new ReviewRevisionsGridDataProvider();
+		parent::SelectableFileListGridHandler(
+			$dataProvider,
+			WORKFLOW_STAGE_ID_INTERNAL_REVIEW,
+			FILE_GRID_DELETE
+		);
 
-	//
-	// Implement template methods from PKPHandler
-	//
-	/**
-	 * @see PKPHandler::authorize()
-	 */
-	function authorize(&$request, &$args, $roleAssignments) {
-		$selectionPolicy =& $this->getSelectionPolicy($request, $args, $roleAssignments);
-		if (!is_null($selectionPolicy)) {
-			$this->addPolicy($selectionPolicy);
-		}
-		return parent::authorize($request, $args, $roleAssignments);
-	}
+		$this->addRoleAssignment(
+			array(ROLE_ID_SERIES_EDITOR, ROLE_ID_PRESS_MANAGER),
+			array('fetchGrid', 'fetchRow', 'downloadAllFiles')
+		);
 
-	/**
-	 * @see PKPHandler::initialize()
-	 */
-	function initialize(&$request) {
-		// Add checkbox column to the grid.
-		import('controllers.grid.files.fileList.FileSelectionGridColumn');
-		$this->addColumn(new FileSelectionGridColumn($this->getSelectName()));
-
-		parent::initialize($request);
-	}
-
-
-	//
-	// Overridden methods from GridHandler
-	//
-	/**
-	 * @see GridHandler::getRequestArgs()
-	 */
-	function getRequestArgs() {
-		$requestArgs = array_merge(parent::getRequestArgs(), $this->getSelectionArgs());
-		return $requestArgs;
-	}
-
-	/**
-	 * @see GridHandler::loadData()
-	 */
-	function &loadData($request, $filter) {
-		// Go through the submission files and set their
-		// "selected" flag.
-		$submissionFiles =& parent::loadData($request, $filter);
-		$selectedFiles =& $this->getSelectedFileIds($submissionFiles);
-		foreach($submissionFiles as $fileId => $submissionFileData) {
-			assert(isset($submissionFileData['submissionFile']));
-			$monographFile =& $submissionFileData['submissionFile']; /* @var $monographFile MonographFile */
-			$submissionFiles[$fileId]['selected'] = in_array(
-				$monographFile->getFileIdAndRevision(),
-				$selectedFiles
-			);
-			unset($monographFile);
-		}
-
-		return $submissionFiles;
-	}
-
-
-	//
-	// Protected methods
-	//
-	/**
-	 * Return an (optional) additional authorization policy
-	 * to authorize the file selection.
-	 * @param $request Request
-	 * @param $args array
-	 * @param $roleAssignments array
-	 * @return PolicySet
-	 */
-	function getSelectionPolicy(&$request, $args, $roleAssignments) {
-		// By default we do not require an additional policy.
-		return null;
-	}
-
-	/**
-	 * Request parameters that describe the selected
-	 * files.
-	 * @param $request Request
-	 * @return array
-	 */
-	function getSelectionArgs() {
-		// By default we do not add any additional
-		// request parameters for the selection.
-		return array();
-	}
-
-	/**
-	 * Get the selected file IDs.
-	 * @param $submissionFiles array Set of submission files to filter
-	 * @return array
-	 */
-	function getSelectedFileIds($submissionFiles) {
-		// By default we select nothing.
-		return array();
-	}
-
-	/**
-	 * Get the selection name.
-	 * @return string
-	 */
-	function getSelectName() {
-		return 'selectedAttachments';
+		// Set the grid title.
+		$this->setTitle('editor.monograph.revisions');
 	}
 }
 
