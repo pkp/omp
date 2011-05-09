117eaeb89b5bea5c2857bab4b5a307d07fd277d1
Matthew Crider <mattcrider@gmail.com>
2011-05-09 15:22:04 -0700

    *6608* Force SSL login problem with base URL override

--- pages/login/LoginHandler.inc.php
+++ pages/login/LoginHandler.inc.php
@@ -83,6 +83,13 @@ class LoginHandler extends PKPLoginHandler {
 
 		Request::redirect(null, 'user');
 	}
+
+	/**
+	 * Get the log in URL.
+	 */
+	function _getLoginUrl() {
+		return Request::url(null, 'login', 'signIn');
+	}
 	
 	/**
 	 * Helper Function - set mail from address
