a7bc3060353adb3b4774960ac79d30e4e619eedd
Alec Smecher <alec@smecher.bc.ca>
2011-06-29 16:41:25 -0700

    *6728* Added MonographFileSignoffDAO helper class

---
+++ classes/monograph/MonographFileSignoffDAO.inc.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * @file classes/monograph/MonographFileSignoffDAO.inc.php
+ *
+ * Copyright (c) 2003-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class MonographFileSignoffDAO
+ * @ingroup monograph
+ * @see SignoffDAO
+ *
+ * @brief Extension of SignoffDAO to work with signoffs relating to monograph
+ * files.
+ */
+
+import('lib.pkp.classes.signoff.SignoffDAO');
+
+class MonographFileSignoffDAO extends SignoffDAO {
+	/**
+	 * Constructor.
+	 */
+	function MonographFileSignoffDAO() {
+		parent::SignoffDAO();
+	}
+
+
+	//
+	// Public methods
+	//
+	/**
+	 * @see SignoffDAO::getById
+	 */
+	function &getById($signoffId) {
+		$returner =& parent::getById($signoffId, ASSOC_TYPE_MONOGRAPH_FILE);
+		return $returner;
+	}
+
+	/**
+	 * Fetch a signoff by symbolic info, building it if needed.
+	 * @param $symbolic string
+	 * @param $monographFileId int
+	 * @param $userId int
+	 * @param $stageId int
+	 * @param $userGroupId int
+	 * @param $fileId int
+	 * @param $fileRevision int
+	 * @return Signoff
+	 */
+	function &build($symbolic, $monographFileId, $userId = null,
+			$userGroupId = null, $fileId = null, $fileRevision = null) {
+		$returner =& parent::build(
+			$symbolic,
+			ASSOC_TYPE_MONOGRAPH_FILE, $monographFileId,
+			$userId, $userGroupId,
+			$fileId, $fileRevision
+		);
+		return $returner;
+	}
+
+	/**
+	 * Determine if a signoff exists
+	 * @param string $symbolic
+	 * @param int $monographFileId
+	 * @param int $stageId
+	 * @param int $userGroupId
+	 * @return boolean
+	 */
+	function signoffExists($symbolic, $monographFileId, $userId = null, $userGroupId = null) {
+		return parent::signoffExists($symbolic, ASSOC_TYPE_MONOGRAPH_FILE, $userId, $userGroupId);
+	}
+
+	/**
+	 * @see SignoffDAO::newDataObject
+	 */
+	function newDataObject() {
+		$signoff = parent::newDataObject();
+		$signoff->setAssocType(ASSOC_TYPE_MONOGRAPH_FILE);
+	}
+
+	/**
+	 * Retrieve the first signoff matching the specified symbolic name and
+	 * monograph file info.
+	 * @param $symbolic string
+	 * @param $monographFileId int
+	 * @param $userId int
+	 * @param $stageId int
+	 * @param $userGroupId int
+	 * @param $fileId int
+	 * @param $fileRevision int
+	 * @return Signoff
+	 */
+	function getBySymbolic($symbolic, $monographFileId, $userId = null,
+			$userGroupId = null, $fileId = null, $fileRevision = null) {
+		$returner = parent::getBySymbolic(
+			$symbolic,
+			ASSOC_TYPE_MONOGRAPH_FILE, $monographFileId,
+			$userId, $userGroupId,
+			$fileId, $fileRevision
+		);
+	}
+}
+
+?>
