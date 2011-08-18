a8ecbed137e20f1258664b6465bdde0883307c4b
Matthew Crider <mattcrider@gmail.com>
2011-08-18 12:51:59 -0700

    Re-added developedBy block plugin to fix layout issue

---
+++ plugins/blocks/developedBy/block.tpl
@@ -0,0 +1,11 @@
+{**
+ * block.tpl
+ *
+ * Copyright (c) 2003-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Common site sidebar menu -- "Developed By" block.
+ *}
+<div class="block" id="sidebarDevelopedBy">
+	<a class="blockTitle" href="http://pkp.sfu.ca/omp/" id="developedBy">{translate key="common.openMonographPress"}</a>
+</div>	
---
+++ plugins/blocks/developedBy/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @defgroup plugins_blocks_developedBy
+ */
+ 
+/**
+ * @file plugins/blocks/developedBy/index.php
+ *
+ * Copyright (c) 2003-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_blocks_developedBy
+ * @brief Wrapper for "developed by" block plugin.
+ *
+ */
+
+
+
+require_once('DevelopedByBlockPlugin.inc.php');
+
+return new DevelopedByBlockPlugin();
+
+?> 
