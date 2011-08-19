69e63be4479edd9ebc65d6ace2beee1e5de06b14
bruno.beghelli <bruno.beghelli@gmail.com>
2011-08-19 09:55:14 -0300

    *6831* Stop using DIRECTORY_SEPARATOR constant

--- plugins/generic/tinymce/TinyMCEPlugin.inc.php
+++ plugins/generic/tinymce/TinyMCEPlugin.inc.php
@@ -16,8 +16,8 @@
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
-define('TINYMCE_INSTALL_PATH', 'lib' . DIRECTORY_SEPARATOR . 'pkp' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'tinymce');
-define('TINYMCE_JS_PATH', TINYMCE_INSTALL_PATH . DIRECTORY_SEPARATOR . 'jscripts' . DIRECTORY_SEPARATOR . 'tiny_mce');
+define('TINYMCE_INSTALL_PATH', 'lib/pkp/lib/tinymce');
+define('TINYMCE_JS_PATH', TINYMCE_INSTALL_PATH . '/jscripts/tiny_mce');
 
 class TinyMCEPlugin extends GenericPlugin {
 	/**
