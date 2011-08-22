6304f4e3c52c7b846ac2e25860ace90f9f4378d5
Juan Pablo Alperin <juan@alperin.ca>
2011-08-22 00:38:00 -0700

    *6430* update NotifyUser Listbuilder

--- controllers/listbuilder/users/NotifyUsersListbuilderHandler.inc.php
+++ controllers/listbuilder/users/NotifyUsersListbuilderHandler.inc.php
@@ -9,7 +9,7 @@
  * @class NotifyUsersListbuilderHandler
  * @ingroup listbuilder
  *
- * @brief Class for adding users to a information center notification
+ * @brief Class for adding participants to a stage.
  */
 
 import('lib.pkp.classes.controllers.listbuilder.ListbuilderHandler');
@@ -20,13 +20,104 @@ class NotifyUsersListbuilderHandler extends ListbuilderHandler {
 	 */
 	function NotifyUsersListbuilderHandler() {
 		parent::ListbuilderHandler();
-		$this->addRoleAssignment(array(ROLE_ID_AUTHOR, ROLE_ID_PRESS_ASSISTANT, ROLE_ID_SERIES_EDITOR, ROLE_ID_PRESS_MANAGER),
-				array('fetch', 'addItem', 'deleteItems'));
+		$this->addRoleAssignment(
+			array(ROLE_ID_PRESS_MANAGER, ROLE_ID_SERIES_EDITOR, ROLE_ID_PRESS_ASSISTANT),
+			array('fetch', 'fetchRow', 'fetchOptions')
+		);
+	}
+
+
+	//
+	// Getters/Setters
+	//
+	/**
+	 * Get the authorized monograph.
+	 * @return Monograph
+	 */
+	function getMonograph() {
+		return $this->getAuthorizedContextObject(ASSOC_TYPE_MONOGRAPH);
 	}
 
 	//
-	// Overridden template methods
+	// Overridden parent class functions
 	//
+	/**
+	 * @see GridDataProvider::getRequestArgs()
+	 */
+	function getRequestArgs() {
+		$monograph =& $this->getMonograph();
+		return array(
+			'monographId' => $monograph->getId()
+		);
+	}
+
+	/**
+	 * @see GridHandler::getRowDataElement
+	 * Get the data element that corresponds to the current request
+	 * Allow for a blank $rowId for when creating a not-yet-persisted row
+	 */
+	function &getRowDataElement(&$request, $rowId) {
+		// fallback on the parent if a rowId is found
+		if ( !empty($rowId) ) {
+			return parent::getRowDataElement($request, $rowId);
+		}
+
+		// Otherwise return from the newRowId
+		// FIXME: Bug #6199; user ID not validated
+		$userId = (int) $this->getNewRowId($request);
+		$userDao =& DAORegistry::getDAO('UserDAO');
+		$user =& $userDao->getUser($userId);
+		return $user;
+	}
+
+	/**
+	 * @see ListbuilderHandler::getOptions
+	 * @params $userGroupId int A user group id to filter by (defaults to URL)
+	 */
+	function getOptions() {
+		// Initialize the object to return
+		$items = array(
+			array()
+		);
+
+		$userStageAssignmentDao =& DAORegistry::getDAO('UserStageAssignmentDAO');
+		$monograph =& $this->getMonograph();
+
+		$users =& $userStageAssignmentDao->getUsersBySubmissionAndStageId($monograph->getId());
+
+		while (!$users->eof()) {
+			$user =& $users->next();
+			$items[0][$user->getId()] = $user->getFullName();
+			unset($user);
+		}
+		unset($users);
+
+		return $items;
+	}
+
+	//
+	// Implement protected template methods from PKPHandler
+	//
+	/**
+	 * @see PKPHandler::initialize()
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+
+		// Load submission-specific translations
+		Locale::requireComponents(array(LOCALE_COMPONENT_PKP_USER));
+
+		// Basic configuration.
+		$this->setSourceType(LISTBUILDER_SOURCE_TYPE_SELECT);
+
+		// Name column
+		$nameColumn = new ListbuilderGridColumn($this, 'name', 'common.name');
+		import('controllers.listbuilder.users.UserListbuilderGridCellProvider');
+		$cellProvider =& new UserListbuilderGridCellProvider();
+		$nameColumn->setCellProvider($cellProvider);
+		$this->addColumn($nameColumn);
+	}
+
 	/**
 	 * @see PKPHandler::authorize()
 	 * @param $request PKPRequest
@@ -39,111 +130,42 @@ class NotifyUsersListbuilderHandler extends ListbuilderHandler {
 		return parent::authorize($request, $args, $roleAssignments);
 	}
 
-	/*
-	 * Configure the grid
-	 * @param PKPRequest $request
+	/**
+	 * @see GridHandler::loadData()
 	 */
-	function initialize(&$request) {
-		parent::initialize($request);
-		// Basic configuration
-		$this->setTitle('');
-		$this->setSourceTitle('');
-		$this->setSourceType(LISTBUILDER_SOURCE_TYPE_SELECT); // Drop-down select
-		$this->setListTitle('');
-
-		$this->loadPossibleItemList($request);
-
-		$this->addColumn(new ListbuilderGridColumn($this, 'item', 'common.name'));
+	function loadData($request, $filter) {
+		$users = array();
+		return $users;
 	}
 
 	/**
-	 * Need to add additional data to the template via the fetch method
-	 * @see Form::fetch()
-	 * @param $args array
-	 * @param $request PKPRequest
+	 * Persist a new entry insert.
+	 * @see Listbuilder::insertEntry
 	 */
-	function fetch($args, &$request) {
-		$router =& $request->getRouter();
+	function insertEntry(&$request, $newRowId) {
+		$monograph =& $this->getMonograph();
+		$monographId = $monograph->getId();
+		$userGroupId = $this->getUserGroupId();
+		$userId = (int) $newRowId;
 
-		$monographId = $request->getUserVar('monographId');
-		$additionalVars = array('monographId' => $monographId,
-			'addUrl' =>  $router->url($request, array(), null, 'addItem', null, array('monographId' => $monographId)),
-			'deleteUrl' => $router->url($request, array(), null, 'deleteItems', null, array('monographId' => $monographId))
-		);
-
-		return parent::fetch($args, &$request, $additionalVars);
+		// Create a new stage assignment.
+		$stageAssignmentDao = & DAORegistry::getDAO('StageAssignmentDAO');
+		$stageAssignmentDao->build($monographId, $this->getStageId(), $userGroupId, $userId);
+		return true;
 	}
-
-
-	//
-	// Public AJAX-accessible functions
-	//
-
-	/*
-	 * Handle adding an item to the list
-	 * @param $args array
-	 * @param $request PKPRequest
-	 */
-	function addItem($args, &$request) {
-		$rowId = 'selectList-' . $this->getId();
-		$userId = (int) $request->getUserVar($rowId);
-
-		$userDao =& DAORegistry::getDAO('UserDAO');
-		$user =& $userDao->getUser($userId);
-		if (!$user) {
-			$json = new JSONMessage(false);
-			return $json;
-		}
-
-		// Return JSON with formatted HTML to insert into list
-		$row =& $this->getRowInstance();
-		$row->setGridId($this->getId());
-		$row->setId($userId);
-		$rowData = array('item' => $user->getFullName());
-		$row->setData($rowData);
-		$row->initialize($request);
-
-		$json = new JSONMessage(true, $this->_renderRowInternally($request, $row));
-		return $json->getString();
-
-	}
-
-
-	/*
-	 * Handle deleting items from the list
-	 */
-	function deleteItems($args, &$request) {
-		$json = new JSONMessage(true);
-		return $json->getString();
-	}
-
 
 	/**
-	 * Override the listbuilder template (the regular one does not fit well in a tabbed modal)
-	 * @return string
+	 * Delete an entry.
+	 * @see Listbuilder::deleteEntry
 	 */
-	function getTemplate() {
-		$this->setTemplate('controllers/informationCenter/notifyListbuilder.tpl');
-		return $this->_template;
-	}
+	function deleteEntry(&$request, $rowId) {
+		$userId = (int) $rowId; // No validation b/c delete is specific
+		$monograph =& $this->getMonograph();
+		$stageAssignmentDao = & DAORegistry::getDAO('StageAssignmentDAO'); /* @var $stageAssignmentDao StageAssignmentDAO */
+		$stageAssignmentDao->deleteByAll($monograph->getId(), $this->getStageId(), $this->getUserGroupId(), $userId);
 
-	/* Load possible items to populate drop-down list with */
-	function loadPossibleItemList(&$request) {
-		$monographId = $request->getUserVar('monographId');
-
-		// Retrieve all users associated with the monograph to populate the drop-down list with
-		$monographDao =& DAORegistry::getDAO('MonographDAO');
-		$monograph =& $monographDao->getMonograph($monographId);
-		$associatedUsers = $monograph->getAssociatedUserIds();
-		$userDao =& DAORegistry::getDAO('UserDAO');
-
-		$itemList = array();
-		foreach($associatedUsers as $item) {
-			$user =& $userDao->getUser($item['id']);
-			$itemList[$user->getId()] = $user->getFullName();
-			unset($item);
-		}
-		$this->setPossibleItemList($itemList);
+		return true;
 	}
 }
+
 ?>
