5c7c6827141b3294e3f474018c3e3b25a27603b1
bruno.beghelli <bruno.beghelli@gmail.com>
2011-10-26 15:50:12 -0200

    Fix: give notification grid access to reviewers

--- controllers/grid/notifications/NotificationsGridHandler.inc.php
+++ controllers/grid/notifications/NotificationsGridHandler.inc.php
@@ -25,7 +25,7 @@ class NotificationsGridHandler extends GridHandler {
 	function NotificationsGridHandler() {
 		parent::GridHandler();
 
-		$this->addRoleAssignment(array(ROLE_ID_PRESS_MANAGER, ROLE_ID_SERIES_EDITOR, ROLE_ID_PRESS_ASSISTANT, ROLE_ID_AUTHOR),
+		$this->addRoleAssignment(array(ROLE_ID_PRESS_MANAGER, ROLE_ID_SERIES_EDITOR, ROLE_ID_PRESS_ASSISTANT, ROLE_ID_AUTHOR, ROLE_ID_REVIEWER),
 		array('fetchGrid'));
 	}
 
