1dd1a2f82c7714101829fa5ef52e1eb22272c849
Alec Smecher <alec@smecher.bc.ca>
2011-10-28 10:29:07 -0700

    Code docs

--- pages/workflow/PublicationFormatHandler.inc.php
+++ pages/workflow/PublicationFormatHandler.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @file controllers/template/workflow/PublicationFormatHandler.inc.php
+ * @file pages/workflow/PublicationFormatHandler.inc.php
  *
  * Copyright (c) 2003-2011 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
