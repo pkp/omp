2f043e1eacaacae333affafa7a97aad1e5ec888c
Alec Smecher <alec@smecher.bc.ca>
2011-11-28 13:48:29 -0800

    *6872* New Releases tab initial implementation

--- pages/catalog/CatalogHandler.inc.php
+++ pages/catalog/CatalogHandler.inc.php
@@ -99,7 +99,18 @@ class CatalogHandler extends Handler {
 	 * @param $request PKPRequest
 	 */
 	function newReleases($args, &$request) {
-		fatalError('UNIMPLEMENTED');
+		$this->_setupMonographsTemplate(true);
+
+		$templateMgr =& TemplateManager::getManager();
+		$press =& $request->getPress();
+
+		// Fetch the monographs to display
+		$publishedMonographDao =& DAORegistry::getDAO('PublishedMonographDAO');
+		$publishedMonographs =& $publishedMonographDao->getNewReleases($press->getId());
+		$templateMgr->assign('publishedMonographs', $publishedMonographs);
+
+		// Display the monograph list
+		$templateMgr->display('catalog/monographs.tpl');
 	}
 
 	/**
