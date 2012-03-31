1fe10a82b25d709046af5acbb853a427e5c0cf19
Alec Smecher <alec@smecher.bc.ca>
2012-03-30 17:17:59 -0700

    *7154* IPN landing page

---
+++ pages/payment/PaymentHandler.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file pages/payment/PaymentHandler.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PaymentHandler
+ * @ingroup pages_payment
+ *
+ * @brief Handle requests for payment functions.
+ */
+
+import('classes.handler.Handler');
+
+class PaymentHandler extends Handler {
+	/**
+	 * Constructor
+	 */
+	function PaymentHandler() {
+		parent::Handler();
+	}
+		 
+	/**
+	 * Pass request to plugin.
+	 * @param $args array
+	 * @param $request PKPRequest
+	 */
+	function plugin($args, &$request) {
+		$paymentMethodPlugins =& PluginRegistry::loadCategory('paymethod');
+		$paymentMethodPluginName = array_shift($args);
+		if (empty($paymentMethodPluginName) || !isset($paymentMethodPlugins[$paymentMethodPluginName])) {
+			$request->redirect(null, null, 'index');
+		}
+
+		$paymentMethodPlugin =& $paymentMethodPlugins[$paymentMethodPluginName];
+		if (!$paymentMethodPlugin->isConfigured()) {
+			$request->redirect(null, null, 'index');
+		}
+
+		$paymentMethodPlugin->handle($args, $request);
+	}
+}
+
+?>
---
+++ pages/payment/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @defgroup pages_payment
+ */
+ 
+/**
+ * @file pages/payment/index.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup pages_payment
+ * @brief Handle requests for interactions between the payment system and external
+ * sites/systems.
+ */
+
+switch ($op) {
+	case 'plugin':
+		define('HANDLER_CLASS', 'PaymentHandler');
+		import('pages.payment.PaymentHandler');
+		break;
+}
+
+?>
