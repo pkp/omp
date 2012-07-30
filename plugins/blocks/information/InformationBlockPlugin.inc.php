ec477f0bf362006447bb5d0b674c66d0a6236a4e
Alec Smecher <alec@smecher.bc.ca>
2012-07-30 14:07:46 -0700

    *7723* Added information block plugin

---
+++ plugins/blocks/information/InformationBlockPlugin.inc.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file plugins/blocks/information/InformationBlockPlugin.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class InformationBlockPlugin
+ * @ingroup plugins_blocks_information
+ *
+ * @brief Class for information block plugin
+ */
+
+import('lib.pkp.classes.plugins.BlockPlugin');
+
+class InformationBlockPlugin extends BlockPlugin {
+	/**
+	 * Install default settings on journal creation.
+	 * @return string
+	 */
+	function getContextSpecificPluginSettingsFile() {
+		return $this->getPluginPath() . '/settings.xml';
+	}
+
+	/**
+	 * Get the display name of this plugin.
+	 * @return String
+	 */
+	function getDisplayName() {
+		return __('plugins.block.information.displayName');
+	}
+
+	/**
+	 * Get a description of the plugin.
+	 */
+	function getDescription() {
+		return __('plugins.block.information.description');
+	}
+
+	/**
+	 * Get the HTML contents for this block.
+	 * @param $templateMgr object
+	 * @return $string
+	 */
+	function getContents(&$templateMgr) {
+		$press =& Request::getPress();
+		if (!$press) return '';
+
+		$templateMgr->assign('forReaders', $press->getLocalizedSetting('readerInformation'));
+		$templateMgr->assign('forAuthors', $press->getLocalizedSetting('authorInformation'));
+		$templateMgr->assign('forLibrarians', $press->getLocalizedSetting('librarianInformation'));
+		return parent::getContents($templateMgr);
+	}
+}
+
+?>
---
+++ plugins/blocks/information/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @defgroup plugins_blocks_information
+ */
+ 
+/**
+ * @file plugins/blocks/information/index.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_blocks_information
+ * @brief Wrapper for information block plugin.
+ *
+ */
+
+require_once('InformationBlockPlugin.inc.php');
+
+return new InformationBlockPlugin();
+
+?> 
