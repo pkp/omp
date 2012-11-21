68e5e123a5ac23db185f8ef34d4777a7332b7afa
Alec Smecher <alec@smecher.bc.ca>
2012-11-21 14:42:57 -0800

    *8030* Centralize context settings

--- classes/press/Press.inc.php
+++ classes/press/Press.inc.php
@@ -28,49 +28,6 @@ class Press extends Context {
 	}
 
 	/**
-	 * Retrieve array of press settings.
-	 * @return array
-	 */
-	function &getSettings() {
-		$pressSettingsDao =& DAORegistry::getDAO('PressSettingsDAO');
-		$settings =& $pressSettingsDao->getPressSettings($this->getData('pressId'));
-		return $settings;
-	}
-
-	/**
-	 * Retrieve a press setting value.
-	 * @param $name string
-	 * @param $locale string
-	 * @return mixed
-	 */
-	function &getSetting($name, $locale = null) {
-		$pressSettingsDao =& DAORegistry::getDAO('PressSettingsDAO');
-		$setting =& $pressSettingsDao->getSetting($this->getId(), $name, $locale);
-		return $setting;
-	}
-
-	/**
-	 * Update a press setting value.
-	 * @param $name string
-	 * @param $value mixed
-	 * @param $type string optional
-	 * @param $isLocalized boolean optional
-	 */
-	function updateSetting($name, $value, $type = null, $isLocalized = false) {
-		$pressSettingsDao =& DAORegistry::getDAO('PressSettingsDAO');
-		return $pressSettingsDao->updateSetting($this->getId(), $name, $value, $type, $isLocalized);
-	}
-
-	function &getLocalizedSetting($name) {
-		$returner = $this->getSetting($name, AppLocale::getLocale());
-		if ($returner === null) {
-			unset($returner);
-			$returner = $this->getSetting($name, AppLocale::getPrimaryLocale());
-		}
-		return $returner;
-	}
-
-	/**
 	 * Get "localized" press page title (if applicable).
 	 * param $home boolean get homepage title
 	 * @return string
@@ -123,6 +80,14 @@ class Press extends Context {
 	 */
 	function getAssocType() {
 		return ASSOC_TYPE_PRESS;
+	}
+
+	/**
+	 * Get the settings DAO for this context object.
+	 * @return DAO
+	 */
+	static function getSettingsDAO() {
+		return DAORegistry::getDAO('PressSettingsDAO');
 	}
 }
 
