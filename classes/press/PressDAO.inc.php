483aa87d065455ab9280d4a3072d8b293388e00e
Alec Smecher <alec@smecher.bc.ca>
2012-11-30 13:12:17 -0800

    Call standardization

--- classes/press/PressDAO.inc.php
+++ classes/press/PressDAO.inc.php
@@ -127,7 +127,7 @@ class PressDAO extends ContextDAO {
 		$pluginSettingsDao->deleteByPressId($pressId);
 
 		$reviewFormDao =& DAORegistry::getDAO('ReviewFormDAO');
-		$reviewFormDao->deleteByAssocId(ASSOC_TYPE_PRESS, $pressId);
+		$reviewFormDao->deleteByAssoc(ASSOC_TYPE_PRESS, $pressId);
 
 		$genreDao =& DAORegistry::getDAO('GenreDAO');
 		$genreDao->deleteByPressId($pressId);
