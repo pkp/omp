9410c4d342a23ac40efc8988e32871f911e8a0e2
Alec Smecher <alec@smecher.bc.ca>
2012-12-03 11:04:55 -0800

    *8042* SettingsDAO revamp continues

--- classes/press/PressSettingsDAO.inc.php
+++ classes/press/PressSettingsDAO.inc.php
@@ -12,13 +12,14 @@
  * @brief Operations for retrieving and modifying press settings.
  */
 
+import('lib.pkp.classes.db.SettingsDAO');
 
-class PressSettingsDAO extends DAO {
+class PressSettingsDAO extends SettingsDAO {
 	/**
 	 * Constructor
 	 */
 	function PressSettingsDAO() {
-		parent::DAO();
+		parent::SettingsDAO();
 	}
 
 	/**
@@ -181,90 +182,6 @@ class PressSettingsDAO extends DAO {
 	}
 
 	/**
-	 * Used internally by installSettings to perform variable and translation replacements.
-	 * @param $rawInput string contains text including variable and/or translate replacements.
-	 * @param $paramArray array contains variables for replacement
-	 * @return string
-	 */
-	function _performReplacement($rawInput, $paramArray = array()) {
-		$value = preg_replace_callback('{{translate key="([^"]+)"}}', '_installer_regexp_callback', $rawInput);
-		foreach ($paramArray as $pKey => $pValue) {
-			$value = str_replace('{$' . $pKey . '}', $pValue, $value);
-		}
-		return $value;
-	}
-
-	/**
-	 * Used internally by installSettings to recursively build nested arrays.
-	 * Deals with translation and variable replacement calls.
-	 * @param $node object XMLNode <array> tag
-	 * @param $paramArray array Parameters to be replaced in key/value contents
-	 */
-	function &_buildObject (&$node, $paramArray = array()) {
-		$value = array();
-		foreach ($node->getChildren() as $element) {
-			$key = $element->getAttribute('key');
-			$childArray =& $element->getChildByName('array');
-			if (isset($childArray)) {
-				$content = $this->_buildObject($childArray, $paramArray);
-			} else {
-				$content = $this->_performReplacement($element->getValue(), $paramArray);
-			}
-			if (!empty($key)) {
-				$key = $this->_performReplacement($key, $paramArray);
-				$value[$key] = $content;
-			} else $value[] = $content;
-		}
-		return $value;
-	}
-
-	/**
-	 * Install press settings from an XML file.
-	 * @param $pressId int ID of press for settings to apply to
-	 * @param $filename string Name of XML file to parse and install
-	 * @param $paramArray array Optional parameters for variable replacement in settings
-	 */
-	function installSettings($pressId, $filename, $paramArray = array()) {
-		$xmlParser = new XMLParser();
-		$tree = $xmlParser->parse($filename);
-
-		if (!$tree) {
-			$xmlParser->destroy();
-			return false;
-		}
-
-		foreach ($tree->getChildren() as $setting) {
-			$nameNode =& $setting->getChildByName('name');
-			$valueNode =& $setting->getChildByName('value');
-
-			if (isset($nameNode) && isset($valueNode)) {
-				$type = $setting->getAttribute('type');
-				$isLocaleField = $setting->getAttribute('locale');
-				$name =& $nameNode->getValue();
-
-				if ($type == 'object') {
-					$arrayNode =& $valueNode->getChildByName('array');
-					$value = $this->_buildObject($arrayNode, $paramArray);
-				} else {
-					$value = $this->_performReplacement($valueNode->getValue(), $paramArray);
-				}
-
-				// Replace translate calls with translated content
-				$this->updateSetting(
-					$pressId,
-					$name,
-					$isLocaleField?array(AppLocale::getLocale() => $value):$value,
-					$type,
-					$isLocaleField
-				);
-			}
-		}
-
-		$xmlParser->destroy();
-
-	}
-
-	/**
 	 * Used internally by reloadLocalizedSettingDefaults to perform variable and translation replacements.
 	 * @param $rawInput string contains text including variable and/or translate replacements.
 	 * @param $paramArray array contains variables for replacement
@@ -415,16 +332,6 @@ class PressSettingsDAO extends DAO {
 		$xmlParser->destroy();
 
 	}
-}
-
-/**
- * Used internally by press setting installation code to perform translation
- * function.
- * @param $matches array
- * @return string
- */
-function _installer_regexp_callback($matches) {
-	return __($matches[1]);
 }
 
 ?>
