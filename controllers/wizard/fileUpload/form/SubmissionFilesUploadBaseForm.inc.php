16f6dfa32dd2cf51de7c797aee2b7c8f446fcfb9
Jason Nugent <jason.nugent@gmail.com>
2013-03-15 10:11:44 -0300

    *8148* use PKP file upload wizard

--- controllers/wizard/fileUpload/form/SubmissionFilesUploadBaseForm.inc.php
+++ controllers/wizard/fileUpload/form/SubmissionFilesUploadBaseForm.inc.php
@@ -13,261 +13,47 @@
  */
 
 
-import('lib.pkp.classes.form.Form');
-import('classes.file.MonographFileManager');
-import('classes.monograph.MonographFile');
+import('lib.pkp.controllers.wizard.fileUpload.form.PKPSubmissionFilesUploadBaseForm');
 
-class SubmissionFilesUploadBaseForm extends Form {
-
-	/** @var integer */
-	var $_stageId;
-
-	/** @var ReviewRound */
-	var $_reviewRound;
-
-	/** @var array the monograph files for this monograph and file stage */
-	var $_monographFiles;
-
+class SubmissionFilesUploadBaseForm extends PKPSubmissionFilesUploadBaseForm {
 
 	/**
 	 * Constructor.
 	 * @param $request Request
 	 * @param $template string
-	 * @param $monographId integer
+	 * @param $submissionId integer
 	 * @param $stageId integer One of the WORKFLOW_STAGE_ID_* constants.
 	 * @param $fileStage integer
 	 * @param $revisionOnly boolean
 	 * @param $reviewRound ReviewRound
 	 * @param $revisedFileId integer
 	 */
-	function SubmissionFilesUploadBaseForm(&$request, $template, $monographId, $stageId, $fileStage,
+	function SubmissionFilesUploadBaseForm(&$request, $template, $submissionId, $stageId, $fileStage,
 			$revisionOnly = false, $reviewRound = null, $revisedFileId = null, $assocType = null, $assocId = null) {
-
-		// Check the incoming parameters.
-		if ( !is_numeric($monographId) || $monographId <= 0 ||
-			!is_numeric($fileStage) || $fileStage <= 0 ||
-			!is_numeric($stageId) || $stageId < 1 || $stageId > 5 ||
-			isset($assocType) !== isset($assocId)) {
-			fatalError('Invalid parameters!');
-		}
-
-		// Initialize class.
-		parent::Form($template);
-		$this->_stageId = $stageId;
-
-		if ($reviewRound) {
-			$this->_reviewRound =& $reviewRound;
-		} else if ($assocType == ASSOC_TYPE_REVIEW_ASSIGNMENT && !$reviewRound) {
-			// Get the review assignment object.
-			$reviewAssignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO'); /* @var $reviewAssignmentDao ReviewAssignmentDAO */
-			$reviewAssignment =& $reviewAssignmentDao->getById((int) $assocId); /* @var $reviewAssignment ReviewAssignment */
-			if ($reviewAssignment->getDateCompleted()) fatalError('Review already completed!');
-
-			// Get the review round object.
-			$reviewRoundDao = DAORegistry::getDAO('ReviewRound');
-			$this->_reviewRound =& $reviewRoundDao->getReviewRoundById($reviewAssignment->getReviewRoundId());
-		} else if (!$assocType && !$reviewRound) {
-			$reviewRound = null;
-		}
-
-		$this->setData('fileStage', (int)$fileStage);
-		$this->setData('monographId', (int)$monographId);
-		$this->setData('revisionOnly', (boolean)$revisionOnly);
-		$this->setData('revisedFileId', $revisedFileId ? (int)$revisedFileId : null);
-		$this->setData('assocType', $assocType ? (int)$assocType : null);
-		$this->setData('assocId', $assocId ? (int)$assocId : null);
-
-		// Add validators.
-		$this->addCheck(new FormValidatorPost($this));
-	}
-
-
-	//
-	// Setters and Getters
-	//
-	/**
-	 * Get the workflow stage id.
-	 * @return integer
-	 */
-	function getStageId() {
-		return $this->_stageId;
+		parent::PKPSubmissionFilesUploadBaseForm($request, $template, $submissionId, $stageId, $fileStage,
+				$revisionOnly, $reviewRound, $revisedFileId, $assocType, $assocId);
 	}
 
 	/**
-	 * Get the review round object (if any).
-	 * @return ReviewRound
+	 * @see PKPSubmissionFilesUploadBaseForm::getSubmissionFiles() for the rest of this.  This function
+	 * exists in this subclass for Monograph-specific submission files.
 	 */
-	function &getReviewRound() {
-		return $this->_reviewRound;
-	}
 
-	/**
-	 * Get the revised file id (if any).
-	 * @return int the revised file id
-	 */
-	function getRevisedFileId() {
-		return $this->getData('revisedFileId') ? (int)$this->getData('revisedFileId') : null;
-	}
-
-	/**
-	 * Get the associated type
-	 * @return integer
-	 */
-	function getAssocType() {
-		return $this->getData('assocType');
-	}
-
-	/**
-	 * Get the associated id.
-	 * @return integer
-	 */
-	function getAssocId() {
-		return $this->getData('assocId');
-	}
-
-	/**
-	 * Get the monograph files belonging to the
-	 * monograph and to the file stage.
-	 * @return array a list of MonographFile instances.
-	 */
-	function &getMonographFiles() {
-		if (is_null($this->_monographFiles)) {
-			$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO'); /* @var $submissionFileDao SubmissionFileDAO */
-			if ($this->getStageId() == WORKFLOW_STAGE_ID_INTERNAL_REVIEW || $this->getStageId() == WORKFLOW_STAGE_ID_EXTERNAL_REVIEW) {
-				// If we have a review stage id then we also expect a review round.
-				if (!is_a($this->getReviewRound(), 'ReviewRound')) assert(false);
-
-				// Can only upload submission files, review files, or review attachments.
-				if (!in_array($this->getData('fileStage'), array(SUBMISSION_FILE_SUBMISSION, SUBMISSION_FILE_REVIEW_FILE, SUBMISSION_FILE_REVIEW_ATTACHMENT, SUBMISSION_FILE_REVIEW_REVISION))) fatalError('Invalid file stage!');
-
-				// Hide the revision selector for review
-				// attachments to make it easier for reviewers
-				if ($this->getData('fileStage') == SUBMISSION_FILE_REVIEW_ATTACHMENT) {
-					$this->_monographFiles = array();
-				} else {
-					// Retrieve the monograph files for the given review round.
-					$reviewRound =& $this->getReviewRound();
-					$this->_monographFiles =& $submissionFileDao->getRevisionsByReviewRound($reviewRound);
-				}
-			} else if ($this->getStageId() == WORKFLOW_STAGE_ID_PRODUCTION &&
+	function &getSubmissionFiles() {
+		if (is_null($this->_submissionFiles)) {
+			if ($this->getStageId() == WORKFLOW_STAGE_ID_PRODUCTION &&
 				$this->getAssocType() == ASSOC_TYPE_PUBLICATION_FORMAT && is_int($this->getAssocId())) {
-				// Retrieve only the monograph files with the same publication format.
-				$this->_monographFiles =& $submissionFileDao->getLatestRevisionsByAssocId(ASSOC_TYPE_PUBLICATION_FORMAT,
-					$this->getAssocId(), $this->getData('monographId'), $this->getData('fileStage'));
+				$submissionFileDao =& DAORegistry::getDAO('SubmissionFileDAO'); /* @var $submissionFileDao SubmissionFileDAO */
+				// Retrieve only the submission files with the same publication format.
+				$this->_submissionFiles =& $submissionFileDao->getLatestRevisionsByAssocId(ASSOC_TYPE_PUBLICATION_FORMAT,
+					$this->getAssocId(), $this->getData('submissionId'), $this->getData('fileStage'));
 			} else {
-				// Retrieve the monograph files for the given file stage.
-				$this->_monographFiles =& $submissionFileDao->getLatestRevisions(
-					$this->getData('monographId'), $this->getData('fileStage')
-				);
+				// Check with the parent class for things besides publication formats.
+				$this->_submissionFiles =& parent::getSubmissionFiles();
 			}
 		}
 
-		return $this->_monographFiles;
-	}
-
-
-	//
-	// Implement template methods from Form
-	//
-	/**
-	 * @see Form::readInputData()
-	 */
-	function readInputData() {
-		// Only Genre and revised file can be set in the form. All other
-		// information is generated on our side.
-		$this->readUserVars(array('revisedFileId'));
-	}
-
-	/**
-	 * @see Form::fetch()
-	 */
-	function fetch($request) {
-		// Set the workflow stage.
-		$this->setData('stageId', $this->getStageId());
-
-		// Set the review round id, if any.
-		$reviewRound =& $this->getReviewRound();
-		if (is_a($reviewRound, 'ReviewRound')) {
-			$this->setData('reviewRoundId', $reviewRound->getId());
-		}
-
-		// Retrieve the uploaded file (if any).
-		$uploadedFile =& $this->getData('uploadedFile');
-
-		// Initialize the list with files available for review.
-		$monographFileOptions = array();
-		$currentMonographFileGenres = array();
-
-		// Go through all files and build a list of files available for review.
-		$revisedFileId = $this->getRevisedFileId();
-		$foundRevisedFile = false;
-		$monographFiles =& $this->getMonographFiles();
-		foreach ($monographFiles as $monographFile) {
-			// The uploaded file must be excluded from the list of revisable files.
-			if ($uploadedFile && $uploadedFile->getFileId() == $monographFile->getFileId()) continue;
-
-			// Is this the revised file?
-			if ($revisedFileId && $revisedFileId == $monographFile->getFileId()) {
-				// This is the revised monograph file, so pass it's data on to the form.
-				$this->setData('revisedFileName', $monographFile->getOriginalFileName());
-				$this->setData('genreId', $monographFile->getGenreId());
-				$foundRevisedFile = true;
-			}
-
-			// Create an entry in the list of existing files which
-			// the user can select from in case he chooses to upload
-			// a revision.
-			$fileName = $monographFile->getLocalizedName() != '' ? $monographFile->getLocalizedName() : __('common.untitled');
-			if ($monographFile->getRevision() > 1) $fileName .= ' (' . $monographFile->getRevision() . ')';
-
-			// If we are about to add a revision of a revision, remove the original one from the list of possible file choices.
-			if (array_key_exists($monographFile->getSourceFileId(), $monographFileOptions)) {
-				unset($monographFileOptions[$monographFile->getSourceFileId()]);
-			}
-
-			$monographFileOptions[$monographFile->getFileId()] = $fileName;
-			$currentMonographFileGenres[$monographFile->getFileId()] = $monographFile->getGenreId();
-
-			$lastMonographFile = $monographFile;
-		}
-
-		// If there is only one option for a file to review, and user must revise, do not show the selector.
-		if (count($monographFileOptions) == 1 && $this->getData('revisionOnly')) {
-			// There was only one option, use the last added monograph file
-			$this->setData('revisedFileId', $lastMonographFile->getFileId());
-			$this->setData('revisedFileName', $lastMonographFile->getOriginalFileName());
-			$this->setData('genreId', $lastMonographFile->getGenreId());
-		}
-
-		// If this is not a "review only" form then add a default item.
-		if (count($monographFileOptions) && !$this->getData('revisionOnly')) {
-			$monographFileOptions = array('' => __('submission.upload.uploadNewFile')) + $monographFileOptions;
-		}
-
-		// Make sure that the revised file (if any) really was among
-		// the retrieved monograph files in the current file stage.
-		if ($revisedFileId && !$foundRevisedFile) fatalError('Invalid revised file id!');
-
-		// Set the review file candidate data in the template.
-		$this->setData('currentMonographFileGenres', $currentMonographFileGenres);
-		$this->setData('monographFileOptions', $monographFileOptions);
-
-		// Show ensuring a blind review link.
-		$press =& $request->getPress();
-		if ($press->getSetting('showEnsuringLink')) {
-			import('lib.pkp.classes.linkAction.request.ConfirmationModal');
-			$ensuringLink = new LinkAction(
-				'addUser',
-				new ConfirmationModal(
-					__('review.blindPeerReview'),
-					__('review.ensuringBlindReview')),
-				__('review.ensuringBlindReview'));
-
-			$templateMgr =& TemplateManager::getManager($request);
-			$templateMgr->assign('ensuringLink', $ensuringLink);
-		}
-
-		return parent::fetch($request);
+		return $this->_submissionFiles;
 	}
 }
 
--- controllers/wizard/fileUpload/form/SubmissionFilesUploadForm.inc.php
+++ controllers/wizard/fileUpload/form/SubmissionFilesUploadForm.inc.php
@@ -13,18 +13,13 @@
  */
 
 
-import('controllers.wizard.fileUpload.form.SubmissionFilesUploadBaseForm');
+import('lib.pkp.controllers.wizard.fileUpload.form.PKPSubmissionFilesUploadForm');
 
-class SubmissionFilesUploadForm extends SubmissionFilesUploadBaseForm {
-
-	/** @var array */
-	var $_uploaderRoles;
-
-
+class SubmissionFilesUploadForm extends PKPSubmissionFilesUploadForm {
 	/**
 	 * Constructor.
 	 * @param $request Request
-	 * @param $monographId integer
+	 * @param $submissionId integer
 	 * @param $uploaderRoles array
 	 * @param $stageId integer One of the WORKFLOW_STAGE_ID_* constants.
 	 * @param $fileStage integer
@@ -33,256 +28,57 @@ class SubmissionFilesUploadForm extends SubmissionFilesUploadBaseForm {
 	 * @param $reviewRound ReviewRound
 	 * @param $revisedFileId integer
 	 */
-	function SubmissionFilesUploadForm(&$request, $monographId, $stageId, $uploaderRoles, $fileStage,
+	function SubmissionFilesUploadForm(&$request, $submissionId, $stageId, $uploaderRoles, $fileStage,
 			$revisionOnly = false, $reviewRound = null, $revisedFileId = null, $assocType = null, $assocId = null) {
-
-		// Initialize class.
-		assert(is_null($uploaderRoles) || (is_array($uploaderRoles) && count($uploaderRoles) >= 1));
-		$this->_uploaderRoles = $uploaderRoles;
-
-		AppLocale::requireComponents(LOCALE_COMPONENT_APP_MANAGER);
-
-		parent::SubmissionFilesUploadBaseForm(
-			$request, 'controllers/wizard/fileUpload/form/fileUploadForm.tpl',
-			$monographId, $stageId, $fileStage, $revisionOnly, $reviewRound, $revisedFileId, $assocType, $assocId
+		parent::PKPSubmissionFilesUploadForm(
+			$request, $submissionId, $stageId, $uploaderRoles, $fileStage, $revisionOnly, $reviewRound, $revisedFileId, $assocType, $assocId
 		);
 	}
-
-
-	//
-	// Getters and Setters
-	//
-	/**
-	 * Get the uploader roles.
-	 * @return array
-	 */
-	function getUploaderRoles() {
-		assert(!is_null($this->_uploaderRoles));
-		return $this->_uploaderRoles;
-	}
-
-
-	//
-	// Implement template methods from Form
-	//
-	/**
-	 * @see Form::readInputData()
-	 */
-	function readInputData() {
-		$this->readUserVars(array('genreId', 'uploaderUserGroupId'));
-		return parent::readInputData();
-	}
-
-	/**
-	 * @see Form::validate()
-	 */
-	function validate(&$request) {
-		// Is this a revision?
-		$revisedFileId = $this->getRevisedFileId();
-		if ($this->getData('revisionOnly')) {
-			assert($revisedFileId > 0);
-		}
-
-		// Retrieve the request context.
-		$router =& $request->getRouter();
-		$context =& $router->getContext($request);
-
-		if (!$revisedFileId) {
-			// Add an additional check for the genre to the form.
-			$this->addCheck(
-				new FormValidatorCustom(
-					$this, 'genreId', FORM_VALIDATOR_REQUIRED_VALUE,
-					'submission.upload.noGenre',
-					create_function(
-						'$genreId,$genreDao,$context',
-						'return is_a($genreDao->getById($genreId, $context->getId()), "Genre");'
-					),
-					array(DAORegistry::getDAO('GenreDAO'), $context)
-				)
-			);
-		}
-
-		// Validate the uploader's user group.
-		$uploaderUserGroupId = $this->getData('uploaderUserGroupId');
-		if ($uploaderUserGroupId) {
-			$user =& $request->getUser();
-			$this->addCheck(
-				new FormValidatorCustom(
-					$this, 'uploaderUserGroupId', FORM_VALIDATOR_REQUIRED_VALUE,
-					'submission.upload.invalidUserGroup',
-					create_function(
-						'$userGroupId,$userGroupDao,$userId',
-						'return $userGroupDao->userInGroup($userId, $userGroupId);'
-					),
-					array(DAORegistry::getDAO('UserGroupDAO'), $user->getId(), $context)
-				)
-			);
-		}
-
-		return parent::validate();
-	}
-
-	/**
-	 * @see Form::fetch()
-	 */
-	function fetch($request) {
-		// Retrieve available monograph file genres.
-		$genreList =& $this->_retrieveGenreList($request);
-		$this->setData('monographFileGenres', $genreList);
-
-		// Retrieve the current context.
-		$router =& $request->getRouter();
-		$context =& $router->getContext($request);
-		assert(is_a($context, 'Press'));
-
-		// Retrieve the user's user groups.
-		$user =& $request->getUser();
-		$userGroupDao = DAORegistry::getDAO('UserGroupDAO'); /* @var $userGroupDao UserGroupDAO */
-		$assignedUserGroups =& $userGroupDao->getByUserId($user->getId(), $context->getId());
-
-		// Check which of these groups make sense in the context
-		// from which the uploader was instantiated.
-		// FIXME: The series editor role may only be displayed if the
-		// user is assigned to the current submission as a series
-		// editor, see #6000.
-		$uploaderRoles = $this->getUploaderRoles();
-		$uploaderUserGroups = array();
-		$highestAuthorityUserGroupId = null;
-		$highestAuthorityRoleId = null;
-		while($userGroup =& $assignedUserGroups->next()) { /* @var $userGroup UserGroup */
-			// Add all user groups that belong to any of the uploader roles.
-			if (in_array($userGroup->getRoleId(), $uploaderRoles)) {
-				$uploaderUserGroups[$userGroup->getId()] = $userGroup->getLocalizedName();
-
-				// Identify the first of the user groups that belongs
-				// to the role with the lowest role id (=highest authority
-				// level). We'll need this information to identify the default
-				// selection, see below.
-				if (is_null($highestAuthorityUserGroupId) || $userGroup->getRoleId() <= $highestAuthorityRoleId) {
-					$highestAuthorityRoleId = $userGroup->getRoleId();
-					if (is_null($highestAuthorityUserGroupId) || $userGroup->getId() < $highestAuthorityUserGroupId) {
-						$highestAuthorityUserGroupId = $userGroup->getId();
-					}
-				}
-			}
-
-			unset($userGroup);
-		}
-		if (empty($uploaderUserGroups)) fatalError('Invalid uploader roles!');
-		$this->setData('uploaderUserGroups', $uploaderUserGroups);
-
-		// Identify the default user group (only required when there is
-		// more than one group).
-		$defaultUserGroupId = null;
-		if (count($uploaderUserGroups) > 1) {
-			// See whether the current user has been assigned as
-			// a workflow stage participant.
-			$stageAssignmentDao = & DAORegistry::getDAO('StageAssignmentDAO'); /* @var $stageAssignmentDao StageAssignmentDAO */
-			$stageAssignments = $stageAssignmentDao->getBySubmissionAndStageId(
-				$this->getData('monographId'),
-				$this->getStageId(),
-				null,
-				$user->getId()
-			);
-
-			while ($stageAssignment =& $stageAssignments->next()) { /* @var $stageSignoff Signoff */
-				if (isset($uploaderUserGroups[$stageAssignment->getUserGroupId()])) {
-					$defaultUserGroupId = $stageAssignment->getUserGroupId();
-					break;
-				}
-			}
-
-			// If we didn't find a corresponding stage signoff then
-			// use the user group with the highest authority as default.
-			if (is_null($defaultUserGroupId)) $defaultUserGroupId = $highestAuthorityUserGroupId;
-		}
-		$this->setData('defaultUserGroupId', $defaultUserGroupId);
-
-		// Include a status message for this installation's max file upload size.
-		$this->setData('maxFileUploadSize', get_cfg_var('upload_max_filesize'));
-
-		return parent::fetch($request);
-	}
-
-	/**
-	 * @see Form::execute()
-	 * @param $request Request
-	 * @return MonographFile if successful, otherwise null
-	 */
-	function &execute($request) {
-		// Identify the file genre and category.
-		$revisedFileId = $this->getRevisedFileId();
-		if ($revisedFileId) {
-			// The file genre and category will be copied over from the revised file.
-			$fileGenre = null;
-		} else {
-			// This is a new file so we need the file genre and category from the form.
-			$fileGenre = $this->getData('genreId') ? (int)$this->getData('genreId') : null;
-		}
-
-		// Retrieve the uploader's user group.
-		$uploaderUserGroupId = $this->getData('uploaderUserGroupId');
-		if (!$uploaderUserGroupId) fatalError('Invalid uploader user group!');
-
-		// Identify the uploading user.
-		$user =& $request->getUser();
-		assert(is_a($user, 'User'));
-
-		$assocType = $this->getData('assocType') ? (int) $this->getData('assocType') : null;
-		$assocId = $this->getData('assocId') ? (int) $this->getData('assocId') : null;
-
-		// Upload the file.
-		$press =& $request->getPress();
-		import('classes.file.MonographFileManager');
-		$monographFileManager = new MonographFileManager($press->getId(), $this->getData('monographId'));
-		$fileStage = $this->getData('fileStage');
-		$monographFile = $monographFileManager->uploadMonographFile(
-			'uploadedFile', $fileStage,
-			$user->getId(), $uploaderUserGroupId, $revisedFileId, $fileGenre, $assocType, $assocId
-		);
-
-		if ($monographFile && ($fileStage == SUBMISSION_FILE_REVIEW_FILE || $fileStage == SUBMISSION_FILE_REVIEW_ATTACHMENT || $fileStage == SUBMISSION_FILE_REVIEW_REVISION)) {
-			// Add the uploaded review file to the review round.
-			$reviewRound =& $this->getReviewRound();
-			$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO');
-			$submissionFileDao->assignRevisionToReviewRound($monographFile->getFileId(), $monographFile->getRevision(), $reviewRound);
-		}
-
-		if ($monographFile) {
-			// log the upload event.
-			import('classes.log.MonographFileLog');
-			import('classes.log.MonographFileEventLogEntry'); // constants
-			$localeKey = $revisedFileId ? 'submission.event.revisionUploaded' : 'submission.event.fileUploaded';
-			$assocType = $revisedFileId ? MONOGRAPH_LOG_FILE_REVISION_UPLOAD : MONOGRAPH_LOG_FILE_UPLOAD;
-			MonographFileLog::logEvent($request, $monographFile, $assocType, $localeKey, array('fileStage' => $fileStage, 'revisedFileId' => $revisedFileId, 'fileId' => $monographFile->getFileId(), 'fileRevision' => $monographFile->getRevision(), 'originalFileName' => $monographFile->getOriginalFileName(), 'submissionId' => $this->getData('monographId'), 'username' => $user->getUsername()));
-		}
-
-		return $monographFile;
-	}
-
 
 	//
 	// Private helper methods
 	//
 	/**
-	 * Retrieve the genre list.
-	 * @param $request Request
-	 * @return array
+	 * Upload the file in an app-specific manner.
+	 * @param PKPRequest $request
+	 * @param PKPUser $user
+	 * @param $uploaderUserGroupId int
+	 * @param $revisedFileId int
+	 * @param $fileGenre int
+	 * @param $assocType int
+	 * @param $assocType int
+	 * @return SubmissionFile
 	 */
-	function &_retrieveGenreList(&$request) {
-		$context =& $request->getContext();
-		$genreDao = DAORegistry::getDAO('GenreDAO'); /* @var $genreDao GenreDAO */
-		$genres =& $genreDao->getEnabledByContextId($context->getId());
+	function _uploadFile($request, $user, $uploaderUserGroupId, $revisedFileId, $fileGenre, $assocType, $assocId) {
+		$press =& $request->getPress();
+		import('classes.file.MonographFileManager');
+		$monographFileManager = new MonographFileManager($press->getId(), $this->getData('submissionId'));
+		$fileStage = $this->getData('fileStage');
+		$submissionFile = $monographFileManager->uploadMonographFile(
+			'uploadedFile', $fileStage,
+			$user->getId(), $uploaderUserGroupId, $revisedFileId, $fileGenre, $assocType, $assocId
+		);
 
-		// Transform the genres into an array and
-		// assign them to the form.
-		$genreList = array();
-		while($genre =& $genres->next()){
-			$genreId = $genre->getId();
-			$genreList[$genreId] = $genre->getLocalizedName();
-			unset($genre);
-		}
-		return $genreList;
+		return $submissionFile;
+	}
+
+	/**
+	 * Log the upload event.
+	 * Must be overridden in subclasses.
+	 * @param PKPRequest $request
+	 * @param PKPUser $user
+	 * @param SubmissionFile $submissionFile
+	 * @param int $assocType
+	 * @param int $revisedFileId
+	 * @param int $fileStage
+	 */
+	function _logEvent($request, $user, $submissionFile, $assocType, $revisedFileId, $fileStage) {
+		// log the upload event.
+		import('classes.log.MonographFileLog');
+		import('classes.log.MonographFileEventLogEntry'); // constants
+		$localeKey = $revisedFileId ? 'submission.event.revisionUploaded' : 'submission.event.fileUploaded';
+		$assocType = $revisedFileId ? MONOGRAPH_LOG_FILE_REVISION_UPLOAD : MONOGRAPH_LOG_FILE_UPLOAD;
+		MonographFileLog::logEvent($request, $submissionFile, $assocType, $localeKey, array('fileStage' => $fileStage, 'revisedFileId' => $revisedFileId, 'fileId' => $submissionFile->getFileId(), 'fileRevision' => $submissionFile->getRevision(), 'originalFileName' => $submissionFile->getOriginalFileName(), 'submissionId' => $this->getData('submissionId'), 'username' => $user->getUsername()));
 	}
 }
 
