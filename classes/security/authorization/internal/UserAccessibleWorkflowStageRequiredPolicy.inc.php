c7293ce9d4d6e4ef77466cd1bfbcd8f006695d13
Jason Nugent <jason.nugent@gmail.com>
2013-03-22 14:13:15 -0300

    *8148* use parent class for generic roles

--- classes/security/authorization/internal/UserAccessibleWorkflowStageRequiredPolicy.inc.php
+++ classes/security/authorization/internal/UserAccessibleWorkflowStageRequiredPolicy.inc.php
@@ -12,56 +12,17 @@
  *
  */
 
-import('lib.pkp.classes.security.authorization.AuthorizationPolicy');
+import('lib.pkp.classes.security.authorization.internal.PKPUserAccessibleWorkflowStageRequiredPolicy');
 
-class UserAccessibleWorkflowStageRequiredPolicy extends AuthorizationPolicy {
-	/** @var PKPRequest */
-	var $_request;
+class UserAccessibleWorkflowStageRequiredPolicy extends PKPUserAccessibleWorkflowStageRequiredPolicy {
 
 	/**
 	 * Constructor
 	 * @param $request PKPRequest
 	 */
 	function UserAccessibleWorkflowStageRequiredPolicy(&$request) {
-		parent::AuthorizationPolicy();
-		$this->_request =& $request;
+		parent::PKPUserAccessibleWorkflowStageRequiredPolicy($request);
 	}
-
-	//
-	// Implement template methods from AuthorizationPolicy
-	//
-	/**
-	 * @see AuthorizationPolicy::effect()
-	 */
-	function effect() {
-		$request =& $this->_request;
-		$press =& $request->getContext();
-		$pressId = $press->getId();
-		$user =& $request->getUser();
-		if (!is_a($user, 'User')) return AUTHORIZATION_DENY;
-
-		$userId = $user->getId();
-		$monograph =& $this->getAuthorizedContextObject(ASSOC_TYPE_MONOGRAPH);
-
-		$userGroupDao = DAORegistry::getDAO('UserGroupDAO');
-		$workflowStages = $userGroupDao->getWorkflowStageTranslationKeys();
-
-		$accessibleWorkflowStages = array();
-		foreach ($workflowStages as $stageId => $translationKey) {
-			$accessibleStageRoles = $this->_getAccessibleStageRoles($userId, $pressId, $monograph, $stageId);
-			if (!empty($accessibleStageRoles)) {
-				$accessibleWorkflowStages[$stageId] = $accessibleStageRoles;
-			}
-		}
-
-		if (empty($accessibleWorkflowStages)) {
-			return AUTHORIZATION_DENY;
-		} else {
-			$this->addAuthorizedContextObject(ASSOC_TYPE_ACCESSIBLE_WORKFLOW_STAGES, $accessibleWorkflowStages);
-			return AUTHORIZATION_PERMIT;
-		}
-	}
-
 
 	//
 	// Private helper methods.
@@ -69,49 +30,37 @@ class UserAccessibleWorkflowStageRequiredPolicy extends AuthorizationPolicy {
 	/**
 	 * Check for roles that give access to the passed workflow stage.
 	 * @param int $userId
-	 * @param int $pressId
-	 * @param Monograph $monograph
+	 * @param int $contextId
+	 * @param Submission $submission
 	 * @param int $stageId
 	 * @return array
 	 */
-	function _getAccessibleStageRoles($userId, $pressId, &$monograph, $stageId) {
+	function _getAccessibleStageRoles($userId, $contextId, &$submission, $stageId) {
 		$stageAssignmentDao = & DAORegistry::getDAO('StageAssignmentDAO'); /* @var $stageAssignmentDao StageAssignmentDAO */
 		$userGroupDao = DAORegistry::getDAO('UserGroupDAO');
 
 		$userRoles = $this->getAuthorizedContextObject(ASSOC_TYPE_USER_ROLES);
 
-		$accessibleStageRoles = array();
+		$accessibleStageRoles = parent::_getAccessibleStageRoles($userId, $contextId, $submission, $stageId);
+
 		foreach ($userRoles as $roleId) {
 			switch ($roleId) {
-				case ROLE_ID_MANAGER:
-					// Press managers have access to all submission stages.
-					$accessibleStageRoles[] = $roleId;
-					break;
-
 				case ROLE_ID_SUB_EDITOR:
 					// The requested submission must be part of their series...
 					// and the requested workflow stage must be assigned to
 					// them in the press settings.
 					import('classes.security.authorization.internal.SeriesAssignmentRule');
-					if (SeriesAssignmentRule::effect($pressId, $monograph->getSeriesId(), $userId) &&
-					$userGroupDao->userAssignmentExists($pressId, $userId, $stageId)) {
+					if (SeriesAssignmentRule::effect($contextId, $submission->getSeriesId(), $userId) &&
+					$userGroupDao->userAssignmentExists($contextId, $userId, $stageId)) {
 						$accessibleStageRoles[] = $roleId;
 					}
 					break;
 
-				case ROLE_ID_ASSISTANT:
-				case ROLE_ID_AUTHOR:
-					// The requested workflow stage has been assigned to them
-					// in the requested submission.
-					$stageAssignments =& $stageAssignmentDao->getBySubmissionAndRoleId($monograph->getId(), $roleId, $stageId, $userId);
-					if(!$stageAssignments->wasEmpty()) {
-						$accessibleStageRoles[] = $roleId;
-					}
-					break;
 				default:
 					break;
 			}
 		}
+
 		return $accessibleStageRoles;
 	}
 }
