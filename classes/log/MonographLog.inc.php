f49a9e1746a9d3002798d8bb7b9a80a2b2930faa
Jason Nugent <jason.nugent@gmail.com>
2013-05-02 14:59:48 -0300

    *8212* move logEvent up

--- classes/log/MonographLog.inc.php
+++ classes/log/MonographLog.inc.php
@@ -9,46 +9,12 @@
  * @class MonographLog
  * @ingroup log
  *
- * @brief Static class for adding / accessing monograph log entries.
+ * @brief Static class for adding / accessing submission log entries.
  */
 
 import('lib.pkp.classes.log.PkpLog');
 
 class MonographLog extends PkpLog {
-	/**
-	 * Add a new event log entry with the specified parameters
-	 * @param $request object
-	 * @param $monograph object
-	 * @param $eventType int
-	 * @param $messageKey string
-	 * @param $params array optional
-	 * @return object MonographLogEntry iff the event was logged
-	 */
-	function logEvent($request, &$monograph, $eventType, $messageKey, $params = array()) {
-		// Create a new entry object
-		$monographEventLogDao = DAORegistry::getDAO('SubmissionEventLogDAO');
-		$entry = $monographEventLogDao->newDataObject();
-
-		// Set implicit parts of the log entry
-		$entry->setDateLogged(Core::getCurrentDate());
-		$entry->setIPAddress($request->getRemoteAddr());
-
-		$user = $request->getUser();
-		if ($user) $entry->setUserId($user->getId());
-
-		$entry->setAssocType(ASSOC_TYPE_MONOGRAPH);
-		$entry->setAssocId($monograph->getId());
-
-		// Set explicit parts of the log entry
-		$entry->setEventType($eventType);
-		$entry->setMessage($messageKey);
-		$entry->setParams($params);
-		$entry->setIsTranslated(0); // Legacy for other apps. All messages use locale keys.
-
-		// Insert the resulting object
-		$monographEventLogDao->insertObject($entry);
-		return $entry;
-	}
 }
 
 ?>
