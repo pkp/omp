20b77e489de1243dde08d576f4fac118f69a9692
Alec Smecher <alec@smecher.bc.ca>
2013-05-09 16:39:23 -0700

    *8212* Review round tab handler to PKP lib

--- controllers/tab/workflow/ReviewRoundTabHandler.inc.php
+++ controllers/tab/workflow/ReviewRoundTabHandler.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @file controllers/tab/settings/ReviewRoundTabHandler.inc.php
+ * @file controllers/tab/workflow/ReviewRoundTabHandler.inc.php
  *
  * Copyright (c) 2003-2013 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
@@ -12,17 +12,18 @@
  * @brief Handle AJAX operations for review round tabs on review stages workflow pages.
  */
 
-// Import the base Handler.
 import('classes.handler.Handler');
-import('lib.pkp.classes.core.JSONMessage');
 
-class ReviewRoundTabHandler extends Handler {
+// Import the base class.
+import('lib.pkp.classes.controllers.tab.workflow.PKPReviewRoundTabHandler');
+
+class ReviewRoundTabHandler extends PKPReviewRoundTabHandler {
 
 	/**
 	 * Constructor
 	 */
 	function ReviewRoundTabHandler() {
-		parent::Handler();
+		parent::PKPReviewRoundTabHandler();
 		$this->addRoleAssignment(
 			array(ROLE_ID_SUB_EDITOR, ROLE_ID_MANAGER, ROLE_ID_ASSISTANT),
 			array('internalReviewRound', 'externalReviewRound')
@@ -42,10 +43,6 @@ class ReviewRoundTabHandler extends Handler {
 		import('classes.security.authorization.WorkflowStageAccessPolicy');
 		$this->addPolicy(new WorkflowStageAccessPolicy($request, $args, $roleAssignments, 'submissionId', $stageId));
 
-		// We need a review round id in request.
-		import('lib.pkp.classes.security.authorization.internal.ReviewRoundRequiredPolicy');
-		$this->addPolicy(new ReviewRoundRequiredPolicy($request, $args));
-
 		return parent::authorize($request, $args, $roleAssignments);
 	}
 
@@ -56,64 +53,6 @@ class ReviewRoundTabHandler extends Handler {
 	 */
 	function internalReviewRound($args, $request) {
 		return $this->_reviewRound($args, $request);
-	}
-
-	/**
-	 * JSON fetch the external review round info (tab).
-	 * @param $args array
-	 * @param $request PKPRequest
-	 */
-	function externalReviewRound($args, $request) {
-		return $this->_reviewRound($args, $request);
-	}
-
-
-	/**
-	 * @see PKPHandler::setupTemplate
-	 */
-	function setupTemplate($request) {
-		AppLocale::requireComponents(LOCALE_COMPONENT_APP_EDITOR);
-		parent::setupTemplate($request);
-	}
-
-
-	//
-	// Private helper methods.
-	//
-	/**
-	 * Internal function to handle both internal and external reviews round info (tab content).
-	 * @param $request PKPRequest
-	 * @param $args array
-	 */
-	function _reviewRound($args, $request) {
-		$this->setupTemplate($request);
-
-		// Retrieve the authorized submission, stage id and review round.
-		$submission = $this->getAuthorizedContextObject(ASSOC_TYPE_SUBMISSION);
-		$stageId = $this->getAuthorizedContextObject(ASSOC_TYPE_WORKFLOW_STAGE);
-		$reviewRound = $this->getAuthorizedContextObject(ASSOC_TYPE_REVIEW_ROUND);
-
-		// Add the round information to the template.
-		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('stageId', $stageId);
-		$templateMgr->assign('reviewRoundId', $reviewRound->getId());
-		$templateMgr->assign('submission', $submission);
-
-		// Assign editor decision actions to the template, only if
-		// user is accessing the last review round for this stage.
-		$reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
-
-		$notificationRequestOptions = array(
-			NOTIFICATION_LEVEL_NORMAL => array(
-				NOTIFICATION_TYPE_REVIEW_ROUND_STATUS => array(ASSOC_TYPE_REVIEW_ROUND, $reviewRound->getId())),
-			NOTIFICATION_LEVEL_TASK => array(
-				NOTIFICATION_TYPE_ALL_REVIEWS_IN => array(ASSOC_TYPE_REVIEW_ROUND, $reviewRound->getId()),
-				NOTIFICATION_TYPE_ALL_REVISIONS_IN => array(ASSOC_TYPE_REVIEW_ROUND, $reviewRound->getId())),
-			NOTIFICATION_LEVEL_TRIVIAL => array()
-		);
-		$templateMgr->assign('reviewRoundNotificationRequestOptions', $notificationRequestOptions);
-
-		return $templateMgr->fetchJson('workflow/reviewRound.tpl');
 	}
 }
 
