1289117f720a7faceb6e23b75fc83465a10c98f2
Alec Smecher <alec@smecher.bc.ca>
2013-05-22 15:21:48 -0700

    *8212* Cleaned up unused constants

--- classes/submission/common/Action.inc.php
+++ classes/submission/common/Action.inc.php
@@ -16,21 +16,6 @@
  * @brief Application-specific submission actions.
  */
 
-
-// These constants are used as search fields for the various submission lists.
-define('SUBMISSION_FIELD_AUTHOR', 1);
-define('SUBMISSION_FIELD_EDITOR', 2);
-define('SUBMISSION_FIELD_TITLE', 3);
-define('SUBMISSION_FIELD_REVIEWER', 4);
-define('SUBMISSION_FIELD_COPYEDITOR', 5);
-define('SUBMISSION_FIELD_LAYOUTEDITOR', 6);
-define('SUBMISSION_FIELD_PROOFREADER', 7);
-
-define('SUBMISSION_FIELD_DATE_SUBMITTED', 4);
-define('SUBMISSION_FIELD_DATE_COPYEDIT_COMPLETE', 5);
-define('SUBMISSION_FIELD_DATE_LAYOUT_COMPLETE', 6);
-define('SUBMISSION_FIELD_DATE_PROOFREADING_COMPLETE', 7);
-
 import('lib.pkp.classes.submission.common.PKPAction');
 
 class Action extends PKPAction {
