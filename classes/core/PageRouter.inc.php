893720fbdf8d675cac96344eb260bc8fe5a725cf
Alec Smecher <alec@smecher.bc.ca>
2013-06-17 17:07:46 -0700

    Clean up old role code

--- classes/core/PageRouter.inc.php
+++ classes/core/PageRouter.inc.php
@@ -24,38 +24,6 @@ class PageRouter extends PKPPageRouter {
 		return array('about', 'announcement', 'help', 'index', 'information', 'rt', '');
 	}
 
-	/**
-	 * Redirect to user home page (or the user group home page if the user has one user group).
-	 * @param $request PKPRequest the request to be routed
-	 */
-	function redirectHome($request) {
-		$userGroupDao = DAORegistry::getDAO('UserGroupDAO');
-		$user = $request->getUser();
-		$userId = $user->getId();
-
-		if ($press = $this->getContext($request, 1)) {
-			// The user is in the press context, see if they have zero or one roles only
-			$userGroups =& $userGroupDao->getByUserId($userId, $press->getId());
-			if($userGroups->getCount() <= 1) {
-				$userGroup =& $userGroups->next();
-				if (!$userGroup || $userGroup->getRoleId() == ROLE_ID_READER) $request->redirect(null, 'index');
-			}
-			$request->redirect(null, 'dashboard');
-		} else {
-			// The user is at the site context, check to see if they are
-			// only registered in one place w/ one role
-			$userGroups =& $userGroupDao->getByUserId($userId, CONTEXT_ID_NONE);
-
-			if($userGroups->getCount() == 1) {
-				$pressDao = DAORegistry::getDAO('PressDAO');
-				$userGroup = $userGroups->next();
-				$press = $pressDao->getById($userGroup->getContextId());
-				if (!isset($press)) $request->redirect('index', 'index');
-				if ($userGroup->getRoleId() == ROLE_ID_READER) $request->redirect(null, 'index');
-			}
-			$request->redirect('index', 'index');
-		}
-	}
 }
 
 ?>
