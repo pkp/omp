ed15973dbf36c9e33391f508854cabd9bc991a3a
Alec Smecher <alec@smecher.bc.ca>
2013-07-29 12:08:46 -0700

    *8353* Allow UI locale override via URL (OMP pre-1.1)

--- classes/i18n/AppLocale.inc.php
+++ classes/i18n/AppLocale.inc.php
@@ -71,10 +71,14 @@ class AppLocale extends PKPLocale {
 			} else {
 				$sessionManager = SessionManager::getManager();
 				$session = $sessionManager->getUserSession();
-				$locale = $session->getSessionVar('currentLocale');
+				$locale = self::$request->getUserVar('uiLocale');
 
 				$press = self::$request->getPress();
 				$site = self::$request->getSite();
+
+				if (!isset($locale)) {
+					$locale = $session->getSessionVar('currentLocale');
+				}
 
 				if (!isset($locale)) {
 					$locale = self::$request->getCookieVar('currentLocale');
