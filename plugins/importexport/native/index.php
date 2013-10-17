45bfce250666e2bf16e9e23b622d97d308683ba2
Alec Smecher <alec@smecher.bc.ca>
2013-10-17 16:50:25 -0700

    Doxygen tweaks

--- plugins/importexport/native/index.php
+++ plugins/importexport/native/index.php
@@ -1,10 +1,6 @@
 <?php
 
 /**
- * @defgroup plugins_importexport_native Native XML import/export plugin
- */
-
-/**
  * @file plugins/importexport/native/index.php
  *
  * Copyright (c) 2003-2013 John Willinsky
