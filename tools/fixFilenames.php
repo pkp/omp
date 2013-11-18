a4d1463cb05be43f3f72bc62f7eaffaa8ae37a76
Alec Smecher <alec@smecher.bc.ca>
2013-11-18 12:24:59 -0800

    *8461* Add filename correction tool

---
+++ tools/fixFilenames.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file tools/fixFilenames.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class fixFilenames
+ * @ingroup tools
+ *
+ * @brief CLI tool to fix pre-1.0 filenames that may have been incorrectly generated. See bug #8461.
+ */
+
+
+
+require(dirname(__FILE__) . '/bootstrap.inc.php');
+
+import('lib.pkp.classes.cliTool.CliTool');
+
+class fixFilenames extends CommandLineTool {
+	/** @var $dryrun boolean True iff the operation should be a "dry run" (no changes made) only. */
+	var $dryrun;
+
+	/**
+	 * Constructor.
+	 * @param $argv array command-line arguments
+	 */
+	function fixFilenames($argv = array()) {
+		parent::CommandLineTool($argv);
+
+		if (($arg = array_pop($this->argv)) == '--dry-run') {
+			$this->dryrun = true;
+		} elseif ($arg == '') {
+			$this->dryrun = false;
+		} else {
+			$this->usage();
+			exit();
+		}
+	}
+
+	/**
+	 * Print command usage information.
+	 */
+	function usage() {
+		echo "Command-line tool for fixing potential incorrectly named files in OMP 1.0\n"
+			. "Usage:\n"
+			. "\t{$this->scriptName} [--dry-run]\n"
+			. "\t\tThe --dry-run option can be used to test without making changes.\n";
+	}
+
+	/**
+	 * Execute upgrade task
+	 */
+	function execute() {
+		import('classes.install.Upgrade');
+		Upgrade::fixFilenames($this->dryrun);
+	}
+}
+
+$tool = new fixFilenames(isset($argv) ? $argv : array());
+$tool->execute();
+
+?>
