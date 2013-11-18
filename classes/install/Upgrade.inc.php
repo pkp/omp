254c85d44dba6a7d8e0eedd3f4a2b29b4caf5ca1
Alec Smecher <alec@smecher.bc.ca>
2013-11-18 12:24:33 -0800

    *8461* Move filename generation to sanity

--- classes/install/Upgrade.inc.php
+++ classes/install/Upgrade.inc.php
@@ -34,6 +34,62 @@ class Upgrade extends Installer {
 		return true;
 	}
 
+
+	//
+	// Specific upgrade actions
+	//
+
+	/**
+	 * Fix broken submission filenames (bug #8461)
+	 * @param $dryrun boolean True iff only a dry run (displaying rather than executing changes) should be done.
+	 * @return boolean
+	 */
+	function fixFilenames($dryrun = false) {
+		$pressDao = DAORegistry::getDAO('PressDAO');
+		$submissionDao = DAORegistry::getDAO('MonographDAO');
+		$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO');
+		$genreDao = DAORegistry::getDAO('GenreDAO');
+
+		import('lib.pkp.classes.file.SubmissionFileManager');
+
+		$contexts = $pressDao->getAll();
+		while ($context = $contexts->next()) {
+			$submissions = $submissionDao->getByPressId($context->getId());
+			while ($submission = $submissions->next()) {
+				$submissionFileManager = new SubmissionFileManager($context->getId(), $submission->getId());
+				$submissionFiles = $submissionFileDao->getBySubmissionId($submission->getId());
+				foreach ($submissionFiles as $submissionFile) {
+					$generatedFilename = $submissionFile->getFileName();
+					$basePath = $submissionFileManager->getBasePath() . $submissionFile->_fileStageToPath($submissionFile->getFileStage()) . '/';
+					$globPattern = $submissionFile->getSubmissionId() . '-' .
+						'*' . '_' . '*' . '-' . // Genre name and designation globbed
+						$submissionFile->getFileId() . '-' .
+						$submissionFile->getRevision() . '-' .
+						$submissionFile->getFileStage() . '-' .
+						date('Ymd', strtotime($submissionFile->getDateUploaded())) .
+						'.' . strtolower_codesafe($submissionFile->getExtension());
+
+					$matchedResults = glob($basePath . $globPattern);
+					if (count($matchedResults)>1) {
+						fatalError("Duplicate potential files for \"$globPattern\"!");
+					}
+					if (count($matchedResults) == 1) {
+						// 1 result matched.
+						$discoveredFilename = array_shift($matchedResults);
+						if ($dryrun) {
+							echo "Need to rename \"$discoveredFilename\" to \"$generatedFilename\".\n";
+						} else {
+							rename($basePath . $discoveredFilename, $basePath . $generatedFilename);
+						}
+					} else {
+						// 0 results matched.
+						die("Unable to find a match for \"$globPattern\".\n");
+					}
+				}
+			}
+		}
+		return true;
+	}
 }
 
 ?>
