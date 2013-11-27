63a498869241ec00160f794ab06afbaf804646c0
Alec Smecher <alec@smecher.bc.ca>
2013-11-27 13:37:30 -0800

    *8484* Add ordering feature to authors grid

--- controllers/grid/users/author/AuthorGridHandler.inc.php
+++ controllers/grid/users/author/AuthorGridHandler.inc.php
@@ -33,7 +33,7 @@ class AuthorGridHandler extends PKPAuthorGridHandler {
 		$this->addRoleAssignment(
 				array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT, ROLE_ID_AUTHOR),
 				array('fetchGrid', 'fetchRow', 'addAuthor', 'editAuthor',
-				'updateAuthor', 'deleteAuthor'));
+				'updateAuthor', 'deleteAuthor', 'saveSequence'));
 		$this->addRoleAssignment(ROLE_ID_REVIEWER, array('fetchGrid', 'fetchRow'));
 		$this->addRoleAssignment(array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT), array('addUser'));
 	}
