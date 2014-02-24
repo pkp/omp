6fddc8b7c3ab16e5da790079ea6a4e9f2cb7cf7f
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-02-24 17:55:45 -0300

    *8521* Introduce acron plugin application side code

---
+++ plugins/generic/acron/AcronPlugin.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file plugins/generic/acron/AcronPlugin.inc.php
+ *
+ * Copyright (c) 2013 Simon Fraser University Library
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AcronPlugin
+ * @ingroup plugins_generic_acron
+ *
+ * @brief Removes dependency on 'cron' for scheduled tasks, including
+ * possible tasks defined by plugins. See the AcronPlugin::parseCrontab
+ * hook implementation.
+ */
+
+import('lib.pkp.plugins.generic.acron.PKPAcronPlugin');
+
+class AcronPlugin extends PKPAcronPlugin {
+
+
+}
+?>
