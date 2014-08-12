633b95e3634c6a774c014c2e4a897ef6ffda3f60
Alec Smecher <alec@smecher.bc.ca>
2014-08-12 16:01:13 -0700

    *8787* Initial submission for OMP

---
+++ tests/data/ContentBaseTestCase.inc.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file tests/data/ContentBaseTestCase.inc.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ContentBaseTestCase
+ * @ingroup tests_data
+ *
+ * @brief Data build suite: Base class for content creation tests
+ */
+
+import('lib.pkp.tests.data.PKPContentBaseTestCase');
+
+class ContentBaseTestCase extends PKPContentBaseTestCase {
+	/**
+	 * Handle any section information on submission step 1
+	 * @return string
+	 */
+	protected function _handleSection($data) {
+		// Page 1
+		if (isset($data['series'])) {
+			$this->waitForElementPresent('id=seriesId');
+			$this->select('id=seriesId', 'label=' . $this->escapeJS($data['series']));
+		}
+	}
+
+	/**
+	 * Get the number of items in the default submission checklist
+	 * @return int
+	 */
+	protected function _getChecklistLength() {
+		return 5;
+	}
+
+	/**
+	 * Get the submission element's name
+	 * @return string
+	 */
+	protected function _getSubmissionElementName() {
+		return 'Book Manuscript';
+	}
+
+	/**
+	 * Send to review.
+	 * @param $type string "External" or "Internal"; type of review.
+	 */
+	protected function sendToReview($type = 'External') {
+		$this->waitForElementPresent('//span[text()=\'Send to ' . $this->escapeJS($type) . ' Review\']/..');
+		$this->click('//span[text()=\'Send to ' . $this->escapeJS($type) . ' Review\']/..');
+		$this->waitForElementPresent('//form[@id=\'initiateReview\']//input[@type=\'checkbox\']');
+		$this->waitForElementPresent('//form[@id=\'initiateReview\']//span[text()=\'Send to ' . $this->escapeJS($type) . ' Review\']/..');
+		$this->click('//form[@id=\'initiateReview\']//span[text()=\'Send to ' . $this->escapeJS($type) . ' Review\']/..');
+		$this->waitForElementNotPresent('css=.ui-widget-overlay');
+	}
+}
