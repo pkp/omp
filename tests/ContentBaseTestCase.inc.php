77b3ed4978b77e4e0fdd6048acd989adbf6a76c3
Alec Smecher <alec@smecher.bc.ca>
2014-10-30 16:28:13 -0700

    *8995* Make submission steps more flexible; introduce EV support

--- tests/ContentBaseTestCase.inc.php
+++ tests/ContentBaseTestCase.inc.php
@@ -20,11 +20,50 @@ class ContentBaseTestCase extends PKPContentBaseTestCase {
 	 * Handle any section information on submission step 1
 	 * @return string
 	 */
-	protected function _handleSection($data) {
+	protected function _handleStep1($data) {
 		// Page 1
 		if (isset($data['series'])) {
 			$this->waitForElementPresent('id=seriesId');
 			$this->select('id=seriesId', 'label=' . $this->escapeJS($data['series']));
+		}
+		switch ($data['type']) {
+			case 'monograph':
+				$this->click('id=isEditedVolume-0');
+				break;
+			case 'editedVolume':
+				$this->click('id=isEditedVolume-1');
+				break;
+			default:
+				fatalError('Unknown submission type.');
+		}
+
+		parent::_handleStep1($data);
+	}
+
+	/**
+	 * Handle any section information on submission step 3
+	 * @return string
+	 */
+	protected function _handleStep3($data) {
+		parent::_handleStep3($data);
+
+		if (isset($data['chapters'])) foreach ($data['chapters'] as $chapter) {
+			$this->click('css=[id^=component-grid-users-chapter-chaptergrid-addChapter-button-]');
+			$this->waitForElementPresent('//form[@id=\'editChapterForm\']//input[starts-with(@id,\'title-\')]');
+			$this->type('//form[@id=\'editChapterForm\']//input[starts-with(@id,\'title-\')]', $chapter['title']);
+			if (isset($chapter['subtitle'])) {
+				$this->type('//form[@id=\'editChapterForm\']//input[starts-with(@id,\'subtitle-\')]', $chapter['subtitle']);
+			}
+
+			// Contributors
+			foreach ($chapter['contributors'] as $contributor) {
+				$this->waitForElementPresent('css=[id^=component-listbuilder-users-chapterauthorlistbuilder-addItem-button-]');
+				$this->clickAt('css=[id^=component-listbuilder-users-chapterauthorlistbuilder-addItem-button-]', '10,10');
+				$this->waitForElementPresent('//select[@name=\'newRowId[name]\']//option[text()=\'' . $contributor . '\']');
+				$this->select('name=newRowId[name]', 'label=' . $contributor);
+			}
+			$this->click('//form[@id=\'editChapterForm\']//span[text()=\'Save\']/..');
+			$this->waitForElementNotPresent('css=.ui-widget-overlay');
 		}
 	}
 
