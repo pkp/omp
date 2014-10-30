a979d1b845a664b1a5ab698c3036985421666a09
Alec Smecher <alec@smecher.bc.ca>
2014-10-30 15:21:07 -0700

    *8995* Added Jbrowser test

---
+++ tests/data/60-content/JbrowerSubmissionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file tests/data/60-content/JbrowerSubmissionTest.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class JbrowerSubmissionTest
+ * @ingroup tests_data
+ *
+ * @brief Data build suite: Create submission
+ */
+
+import('tests.data.ContentBaseTestCase');
+
+class JbrowerSubmissionTest extends ContentBaseTestCase {
+	/**
+	 * Create a submission.
+	 */
+	function testSubmission() {
+		$this->register(array(
+			'username' => 'jbrower',
+			'firstName' => 'Jennifer',
+			'lastName' => 'Brower',
+			'affiliation' => 'Buffalo National Park Foundation',
+			'country' => 'Canada',
+			'roles' => array('Author'),
+		));
+
+		$this->createSubmission(array(
+			'type' => 'monograph',
+			'title' => 'Lost Tracks: Buffalo National Park, 1909-1939',
+			'abstract' => 'While contemporaries and historians alike hailed the establishment of Buffalo National Park in Wainwright, Alberta as a wildlife saving effort, the political climate of the early 20th century worked against it. The Canadian Parks Branch was never sufficiently funded to operate BNP effectively or to remedy the crises the animals faced as a result. Cross-breeding experiments with bison and domestic cattle proved unfruitful. Attempts at commercializing the herd had no success. Ultimately, the Department of National Defence repurposed the park for military training and the bison disappeared once more.',
+			'keywords' => array(
+				'Biography & Memoir',
+				'Environmental Studies',
+				'Political & International Studies',
+			),
+		));
+
+		$this->logOut();
+	}
+}
