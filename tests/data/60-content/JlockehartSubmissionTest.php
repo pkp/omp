d26256c53bed3f1598a7edabe34657bebd75e6b1
Alec Smecher <alec@smecher.bc.ca>
2014-11-03 16:39:00 -0800

    *8995* Added Jloclehart submission

---
+++ tests/data/60-content/JlockehartSubmissionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file tests/data/60-content/JlockehartSubmissionTest.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class JlockehartSubmissionTest
+ * @ingroup tests_data
+ *
+ * @brief Data build suite: Create submission
+ */
+
+import('tests.data.ContentBaseTestCase');
+
+class JlockehartSubmissionTest extends ContentBaseTestCase {
+	/**
+	 * Create a submission.
+	 */
+	function testSubmission() {
+		$this->register(array(
+			'username' => 'jlockehart',
+			'firstName' => 'Jonathan',
+			'lastName' => 'Locke Hart',
+			// 'affiliation' => '',
+			'country' => 'Canada',
+			'roles' => array('Author'),
+		));
+
+		$title = 'Dreamwork';
+		$this->createSubmission(array(
+			'type' => 'monograph',
+			'title' => $title,
+			'abstract' => 'Dreamwork is a poetic exploration of the then and there, here and now, of landscapes and inscapes over time. It is part of a poetry series on dream and its relation to actuality. The poems explore past, present, and future in different places from Canada through New Jersey, New York and New England to England and Europe, part of the speaker’s journey. A typology of home and displacement, of natural beauty and industrial scars unfolds in the movement of the book.',
+		));
+
+		$this->logOut();
+	}
+}
