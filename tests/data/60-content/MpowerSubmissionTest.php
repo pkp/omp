59ff62662391028f00fce3ab89b93b8af4c6a20e
Alec Smecher <alec@smecher.bc.ca>
2014-11-03 16:34:03 -0800

    *8995* Added Mpower submission

---
+++ tests/data/60-content/MpowerSubmissionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file tests/data/60-content/MpowerSubmissionTest.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class MpowerSubmissionTest
+ * @ingroup tests_data
+ *
+ * @brief Data build suite: Create submission
+ */
+
+import('tests.data.ContentBaseTestCase');
+
+class MpowerSubmissionTest extends ContentBaseTestCase {
+	/**
+	 * Create a submission.
+	 */
+	function testSubmission() {
+		$this->register(array(
+			'username' => 'mpower',
+			'firstName' => 'Michael',
+			'lastName' => 'Power',
+			// 'affiliation' => '',
+			'country' => 'Canada',
+			'roles' => array('Author'),
+		));
+
+		$title = 'A Designer\'s Log: Case Studies in Instructional Design';
+		$this->createSubmission(array(
+			'type' => 'monograph',
+			'title' => $title,
+			'abstract' => 'Books and articles on instructional design in online learning abound but rarely do we get such a comprehensive picture of what instructional designers do, how they do it, and the problems they solve as their university changes. Power documents the emergence of an adapted instructional design model for transforming courses from single-mode to dual-mode instruction, making this designer’s log a unique contribution to the fi eld of online learning.',
+		));
+
+		$this->logOut();
+	}
+}
