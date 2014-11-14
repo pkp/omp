c27debf4bfdd73857971cc13365e7066aabd97d7
Alec Smecher <alec@smecher.bc.ca>
2014-11-13 21:29:20 -0800

    Use consistent function descriptors (references)

--- controllers/submission/CategoriesListbuilderHandler.inc.php
+++ controllers/submission/CategoriesListbuilderHandler.inc.php
@@ -134,7 +134,7 @@ class CategoriesListbuilderHandler extends ListbuilderHandler {
 	 * Get the data element that corresponds to the current request
 	 * Allow for a blank $rowId for when creating a not-yet-persisted row
 	 */
-	function getRowDataElement($request, $rowId) {
+	function getRowDataElement($request, &$rowId) {
 		// fallback on the parent if a rowId is found
 		if ( !empty($rowId) ) {
 			return parent::getRowDataElement($request, $rowId);
