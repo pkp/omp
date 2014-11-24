c8518d33da8887030e0017112313f99de453af0a
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-11-24 15:45:38 -0200

    *9013* Created review test in application

---
+++ tests/functional/workflow/ReviewTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file tests/functional/workflow/ReviewTest.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ReviewTest
+ * @ingroup tests_functional_workflow
+ *
+ * @brief Test review process.
+ */
+
+import('lib.pkp.tests.functional.workflow.PKPReviewBaseTestCase');
+
+class ReviewTest extends PKPReviewBaseTestCase {
+
+	/**
+	 * @copydoc PKPReviewTest::testPerformReview()
+	 */
+	public function testPerformReview() {
+		parent::testPerformReview();
+	}
+}
