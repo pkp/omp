46b3e41b93fd9d1208108971b7b802134e12684b
Alec Smecher <alec@smecher.bc.ca>
2014-12-09 16:33:01 -0800

    *9040* Remove underscore prefix from function name

--- controllers/api/file/ManageFileApiHandler.inc.php
+++ controllers/api/file/ManageFileApiHandler.inc.php
@@ -76,8 +76,8 @@ class ManageFileApiHandler extends PKPManageFileApiHandler {
 	 * Get the list of notifications to be updated on metadata form submission.
 	 * @return array
 	 */
-	protected function _getUpdateNotifications() {
-		$updateNotifications = parent::_getUpdateNotifications();
+	protected function getUpdateNotifications() {
+		$updateNotifications = parent::getUpdateNotifications();
 		$updateNotifications[] = NOTIFICATION_TYPE_PENDING_INTERNAL_REVISIONS;
 		return $updateNotifications;
 	}
