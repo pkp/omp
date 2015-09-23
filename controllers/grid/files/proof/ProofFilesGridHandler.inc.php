0826d25b2383370d38e28060ddffeda1e7efa8d7
Alec Smecher <alec@smecher.bc.ca>
2015-09-22 17:00:15 -0700

    Fix authorization of proof files grid

--- controllers/grid/files/proof/ProofFilesGridHandler.inc.php
+++ controllers/grid/files/proof/ProofFilesGridHandler.inc.php
@@ -23,7 +23,8 @@ class ProofFilesGridHandler extends FileListGridHandler {
 		import('lib.pkp.controllers.grid.files.proof.ProofFilesGridDataProvider');
 		parent::FileListGridHandler(
 			new ProofFilesGridDataProvider(),
-				FILE_GRID_ADD|FILE_GRID_MANAGE|FILE_GRID_DELETE|FILE_GRID_VIEW_NOTES|FILE_GRID_EDIT
+			WORKFLOW_STAGE_ID_PRODUCTION,
+			FILE_GRID_ADD|FILE_GRID_MANAGE|FILE_GRID_DELETE|FILE_GRID_VIEW_NOTES|FILE_GRID_EDIT
 		);
 
 		$this->addRoleAssignment(
@@ -68,6 +69,23 @@ class ProofFilesGridHandler extends FileListGridHandler {
 		$this->setId('proofFiles-' . $representation->getId());
 		$this->setTitle('submission.pageProofs');
 		$this->setInstructions('monograph.proofReadingDescription');
+	}
+
+	/**
+	 * Authorize the request.
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 * @return boolean
+	 */
+	function authorize($request, $args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.SubmissionAccessPolicy');
+		$this->addPolicy(new SubmissionAccessPolicy($request, $args, $roleAssignments));
+
+		import('lib.pkp.classes.security.authorization.internal.RepresentationRequiredPolicy');
+		$this->addPolicy(new RepresentationRequiredPolicy($request, $args));
+
+		return parent::authorize($request, $args, $roleAssignments);
 	}
 
 
