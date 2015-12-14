f9f004864bb58a07c2a54447ecc2a617337d169a
Nate Wright <natew@notthisway.com>
2015-12-14 14:20:45 +0000

    Remove NoActionMessage grid logic

--- controllers/grid/files/proof/ApprovedProofFilesGridRow.inc.php
+++ controllers/grid/files/proof/ApprovedProofFilesGridRow.inc.php
@@ -61,8 +61,6 @@ class ApprovedProofFilesGridRow extends GridRow {
 					'edit'
 				)
 			);
-		} else {
-			$this->setNoActionMessage(__('grid.catalogEntry.availableRepresentation.proofNotApproved'));
 		}
 	}
 }
