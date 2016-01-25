a1cda2327255f6f5f611bbbe9c79cda00b2d30be
Alec Smecher <alec@smecher.bc.ca>
2016-01-25 14:35:29 -0800

    pkp/pkp-lib#1085 Update copyright dates to 2016

--- plugins/importexport/onix30/Onix30ExportPlugin.inc.php
+++ plugins/importexport/onix30/Onix30ExportPlugin.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file plugins/importexport/onix30/Onix30ExportPlugin.inc.php
  *
- * Copyright (c) 2014-2015 Simon Fraser University Library
- * Copyright (c) 2003-2015 John Willinsky
+ * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class Onix30ExportPlugin
