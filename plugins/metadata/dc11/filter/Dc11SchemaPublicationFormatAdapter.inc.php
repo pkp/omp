ee698d9a9900a8c45e0c534dc3514dd1b0649baa
Alec Smecher <alec@smecher.bc.ca>
2016-02-29 16:10:36 -0800

    pkp/pkp-lib#1143 Collapse extra coverage fields into one

--- plugins/metadata/dc11/filter/Dc11SchemaPublicationFormatAdapter.inc.php
+++ plugins/metadata/dc11/filter/Dc11SchemaPublicationFormatAdapter.inc.php
@@ -168,10 +168,7 @@ class Dc11SchemaPublicationFormatAdapter extends MetadataDataObjectAdapter {
 		// Relation
 
 		// Coverage
-		$coverage = array_merge_recursive(
-				(array) $monograph->getCoverageGeo(null),
-				(array) $monograph->getCoverageChron(null),
-				(array) $monograph->getCoverageSample(null));
+		$coverage = (array) $monograph->getCoverageGeo(null);
 		$this->_addLocalizedElements($dc11Description, 'dc:coverage', $coverage);
 
 		// Rights
