4824358a68f0058c0205740bc4ceee799fb5423e
Nate Wright <natew@notthisway.com>
2016-05-16 17:52:53 +0100

    pkp/pkp-lib#1461 Fix create press test

--- tests/data/20-CreatePressTest.php
+++ tests/data/20-CreatePressTest.php
@@ -54,7 +54,7 @@ class CreatePressTest extends WebTestCase {
 		$this->open(self::$baseUrl);
 
 		// Management > Settings > Press
-		$this->waitForElementPresent($selector='css=li.profile a:contains(\'Submissions\')');
+		$this->waitForElementPresent($selector='css=li.profile a:contains(\'Dashboard\')');
 		$this->clickAndWait($selector);
 		$this->waitForElementPresent($selector='css=ul#navigationPrimary a:contains(\'Press\')');
 		$this->clickAndWait($selector);
