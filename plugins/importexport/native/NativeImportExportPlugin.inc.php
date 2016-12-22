acda5f1508510757e3db1162830968cbbf7190ab
Bozana Bokan <bozana.bokan@posteo.net>
2016-12-22 10:19:59 +0100

    pkp/pkp-lib#2117 fix space and parameter

--- plugins/importexport/native/NativeImportExportPlugin.inc.php
+++ plugins/importexport/native/NativeImportExportPlugin.inc.php
@@ -194,16 +194,16 @@ class NativeImportExportPlugin extends ImportExportPlugin {
 			if ($submission) $submissions[] = $submission;
 		}
 		libxml_use_internal_errors(true);
-		$submissionXml = $exportFilter->execute($submissions);
+		$submissionXml = $exportFilter->execute($submissions, true);
 		$xml = $submissionXml->saveXml();
-		$errors = array_filter(libxml_get_errors(), create_function('$a', 'return $a->level == LIBXML_ERR_ERROR ||  $a->level == LIBXML_ERR_FATAL;'));
+		$errors = array_filter(libxml_get_errors(), create_function('$a', 'return $a->level == LIBXML_ERR_ERROR || $a->level == LIBXML_ERR_FATAL;'));
 		if (!empty($errors)) {
 			$charset = Config::getVar('i18n', 'client_charset');
 			header('Content-type: text/html; charset=' . $charset);
 			echo '<html><body>';
 			$this->displayXMLValidationErrors($errors, $xml);
-			fatalError(__('plugins.importexport.common.error.validation'));
 			echo '</body></html>';
+			fatalError(__('plugins.importexport.common.error.validation'));
 		}
 		return $xml;
 	}
