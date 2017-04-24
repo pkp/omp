2a91a76034be02dcac3f65460f6972341883b151
Nate Wright <natew@notthisway.com>
2017-04-24 14:07:26 +0100

    pkp/pkp-lib#2453 Fix tests due to change in editorial decision label

--- tests/data/60-content/MallySubmissionTest.php
+++ tests/data/60-content/MallySubmissionTest.php
@@ -103,7 +103,7 @@ class MallySubmissionTest extends ContentBaseTestCase {
 
 		// Accept submission
 		$this->findSubmissionAsEditor('dbarnes', null, $title);
-		$this->recordEditorialDecision('Send to Copyediting');
+		$this->recordEditorialDecision('Accept Submission');
 		$this->waitForElementPresent('//a[contains(text(), \'Copyediting\')]/*[contains(text(), \'Initiated\')]');
 		$this->logOut();
 	}
