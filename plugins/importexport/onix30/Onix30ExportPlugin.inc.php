4d1295e1d64da7e009e80b3c0a2ad295cc61419f
Nate Wright <natew@notthisway.com>
2017-05-09 15:21:14 +0100

    pkp/pkp-lib#2163 Get importexport plugins working with new submissions list

--- plugins/importexport/onix30/Onix30ExportPlugin.inc.php
+++ plugins/importexport/onix30/Onix30ExportPlugin.inc.php
@@ -92,6 +92,14 @@ class Onix30ExportPlugin extends ImportExportPlugin {
 		switch (array_shift($args)) {
 			case 'index':
 			case '':
+				import('lib.pkp.controllers.list.submissions.SelectSubmissionsListHandler');
+				$exportSubmissionsListHandler = new SelectSubmissionsListHandler(array(
+					'title' => 'plugins.importexport.onix30.exportSubmissionsSelect',
+					'count' => 100,
+					'inputName' => 'selectedSubmissions[]',
+					'lazyLoad' => true,
+				));
+				$templateMgr->assign('exportSubmissionsListData', json_encode($exportSubmissionsListHandler->getConfig()));
 				$templateMgr->display($this->getTemplatePath() . 'index.tpl');
 				break;
 			case 'export':
