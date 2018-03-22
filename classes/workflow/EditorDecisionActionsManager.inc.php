a9e37ae6e94e041cc63f3df7011516858c60dde1
Bozana Bokan <bozana.bokan@posteo.net>
2018-03-22 13:11:48 +0100

    pkp/pkp-lib#3436 change array_merge into + to keep the keys

--- classes/workflow/EditorDecisionActionsManager.inc.php
+++ classes/workflow/EditorDecisionActionsManager.inc.php
@@ -154,7 +154,7 @@ class EditorDecisionActionsManager {
 			),
 		);
 		if ($makeDecision) {
-			$decisions = array_merge($decisions, array(
+			$decisions = $decisions + array(
 				SUBMISSION_EDITOR_DECISION_ACCEPT => array(
 					'name' => 'accept',
 					'operation' => 'promote',
@@ -166,7 +166,7 @@ class EditorDecisionActionsManager {
 					'operation' => 'sendReviews',
 					'title' => 'editor.submission.decision.decline',
 				),
-			));
+			);
 		}
 		return $decisions;
 	}
