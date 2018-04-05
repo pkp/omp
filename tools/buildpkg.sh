a625b7df941e6e7306ad77e1a8dc017ed8602feb
Alec Smecher <alec@smecher.bc.ca>
2018-04-05 16:07:13 -0700

    Don't exclude symfony

--- tools/buildpkg.sh
+++ tools/buildpkg.sh
@@ -79,7 +79,6 @@ lib/pkp/lib/vendor/phpmailer/phpmailer/examples						\
 lib/pkp/lib/vendor/phpmailer/phpmailer/test						\
 lib/pkp/lib/vendor/robloach								\
 lib/pkp/lib/vendor/smarty/smarty/demo							\
-lib/pkp/lib/vendor/symfony								\
 lib/pkp/lib/vendor/phpunit								\
 lib/pkp/lib/vendor/phpdocumentor/reflection-docblock					\
 lib/pkp/lib/vendor/doctrine/instantiator/tests						\
