94c57849f715f34fc110da4e1e7017ee296336d4
Alec Smecher <alec@smecher.bc.ca>
2018-11-30 10:28:57 -0800

    pkp/pkp-lib#4277 Behave better when returning empty results

--- classes/monograph/PublishedMonographDAO.inc.php
+++ classes/monograph/PublishedMonographDAO.inc.php
@@ -16,6 +16,7 @@
 
 import('classes.monograph.PublishedMonograph');
 import('classes.monograph.MonographDAO');
+import('lib.pkp.classes.core.ArrayItemIterator');
 
 define('ORDERBY_DATE_PUBLISHED', 'datePublished');
 define('ORDERBY_TITLE', 'title');
@@ -400,7 +401,8 @@ class PublishedMonographDAO extends MonographDAO {
 
 		// If no associated object is passed, return.
 		if (!$assocId || !$assocType) {
-			return new DAOResultFactory();
+			$theArray = array();
+			return new ArrayItemIterator($theArray);
 		} else {
 			// Check if the associated object exists.
 			switch ($assocType) {
@@ -417,8 +419,8 @@ class PublishedMonographDAO extends MonographDAO {
 					$assocObject = null;
 			}
 			if (!$assocObject) {
-				assert(false);
-				return new DAOResultFactory();
+				$theArray = array();
+				return new ArrayItemIterator($theArray);
 			}
 		}
 
