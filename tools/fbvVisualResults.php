eb6b362e12b99351156f24284a4b21e28377de96
Alec Smecher <alec@smecher.bc.ca>
2020-01-15 18:51:20 -0800

    Modernize calls to deprecated function

--- tools/fbvVisualResults.php
+++ tools/fbvVisualResults.php
@@ -19,7 +19,7 @@ define('INDEX_FILE_LOCATION', dirname(dirname(__FILE__)) . '/index.php');
 chdir(dirname(INDEX_FILE_LOCATION)); /* Change to base directory */
 require('lib/pkp/includes/bootstrap.inc.php');
 
-$application = Application::getApplication();
+$application = Application::get();
 $request = $application->getRequest();
 
 // FIXME: Write and use a CLIRouter here (see classdoc)
