f1045e11a0d2e631765f42f394aee594012a6907
Alec Smecher <alec@smecher.bc.ca>
2020-02-28 19:54:53 -0800

    Add exclusions

--- tools/buildpkg.sh
+++ tools/buildpkg.sh
@@ -68,6 +68,8 @@ lib/pkp/lib/vendor/phpmailer/phpmailer/examples						\
 lib/pkp/lib/vendor/phpmailer/phpmailer/test						\
 lib/pkp/lib/vendor/robloach								\
 lib/pkp/lib/vendor/smarty/smarty/demo							\
+lib/pkp/lib/vendor/pimple/pimple/ext/pimple/tests					\
+lib/pkp/lib/vendor/adodb/adodb-php/.git							\
 lib/pkp/lib/vendor/phpunit								\
 lib/pkp/lib/vendor/phpdocumentor/reflection-docblock					\
 lib/pkp/lib/vendor/doctrine/instantiator/tests						\
