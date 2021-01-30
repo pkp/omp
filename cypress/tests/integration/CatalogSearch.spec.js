7febe5d8575311505ee55a0319cf5d602daf3721
Alec Smecher <alec@smecher.bc.ca>
2021-01-29 17:46:11 -0800

    Copyright dates

--- cypress/tests/integration/CatalogSearch.spec.js
+++ cypress/tests/integration/CatalogSearch.spec.js
@@ -1,8 +1,8 @@
 /**
  * @file cypress/tests/integration/CatalogSearch.spec.js
  *
- * Copyright (c) 2016-2020 Simon Fraser University
- * Copyright (c) 2000-2020 John Willinsky
+ * Copyright (c) 2016-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @brief Test for catalog search
--- js/controllers/monographList/MonographListHandler.js
+++ js/controllers/monographList/MonographListHandler.js
@@ -6,8 +6,8 @@
  * @class MonographListHandler
  * @ingroup js_controllers_monographList
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2000-2020 John Willinsky
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @brief Base handler for monograph list.
