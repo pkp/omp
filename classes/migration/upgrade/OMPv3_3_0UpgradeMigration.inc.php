d195137d0d3d47db901320ff5faa3c731cd2dfe9
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2021-09-03 14:22:05 +0300

    pkp/pkp-lib#7250 Check DAO class name before retrival from Registry

--- classes/migration/upgrade/OMPv3_3_0UpgradeMigration.inc.php
+++ classes/migration/upgrade/OMPv3_3_0UpgradeMigration.inc.php
@@ -11,11 +11,13 @@
  * @brief Describe database table structures.
  */
 
+use APP\core\Application;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-
+use PKP\core\EntityDAO;
 use PKP\services\PKPSchemaService;
 use PKP\submission\SubmissionFile;
 
@@ -87,13 +89,28 @@ class OMPv3_3_0UpgradeMigration extends Migration
             \APP\submission\DAO::class
         ];
         $processedTables = [];
+        $application = Application::get();
         foreach ($schemaDAOs as $daoName) {
-            $dao = DAORegistry::getDAO($daoName);
+            $dao = null;
+            if ($application->getQualifiedDAOName($daoName)) {
+                $dao = DAORegistry::getDAO($daoName);
+            }
+
+            // Account for new EntityDAOs
+            if (!$dao) {
+                $dao = App::make($daoName);
+                if (!$dao) {
+                    throw new Exception("${daoName} could not be created when migrating serialized settings");
+                }
+            }
             $schemaService = Services::get('schema');
 
             if (is_a($dao, 'SchemaDAO')) {
                 $schema = $schemaService->get($dao->schemaName);
                 $tableName = $dao->settingsTableName;
+            } elseif (is_a($dao, EntityDAO::class)) {
+                $schema = $schemaService->get($dao->schema);
+                $tableName = $dao->settingsTable;
             } elseif ($daoName === 'SiteDAO') {
                 $schema = $schemaService->get(PKPSchemaService::SCHEMA_SITE);
                 $tableName = 'site_settings';
