0552c5e4f508e722f393e50a95bf5be01b4d9cd1
marsilius <marsilius@ub.uni-heidelberg.de>
2021-11-15 15:12:11 +0100

    pkp/pkp-lib#7132 Add use statements 2

--- classes/publicationFormat/IdentificationCodeDAO.inc.php
+++ classes/publicationFormat/IdentificationCodeDAO.inc.php
@@ -19,7 +19,7 @@ namespace APP\publicationFormat;
 
 use APP\publicationFormat\IdentificationCode;
 
-use HookRegistry;
+use PKP\plugins\HookRegistry;
 use PKP\db\DAOResultFactory;
 
 class IdentificationCodeDAO extends \PKP\db\DAO
