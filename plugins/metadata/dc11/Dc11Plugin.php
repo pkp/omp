c70676808cd1f66886f47b6275f40bad7a4e1d8d
Alec Smecher <alec@smecher.bc.ca>
2022-08-12 20:00:59 -0700

    pkp/pkp-lib#6091 Rename DC11 plugin

---
+++ plugins/metadata/dc11/Dc11Plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file plugins/metadata/dc11/Dc11Plugin.php
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class Dc11Plugin
+ * @brief Dublic Core version 1.1 metadata plugin
+ */
+
+namespace APP\plugins\metadata\dc11;
+
+class Dc11Plugin extends \PKP\plugins\metadata\dc11\PKPDc11MetadataPlugin
+{
+}
