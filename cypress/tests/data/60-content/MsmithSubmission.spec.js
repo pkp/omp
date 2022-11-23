0c328ff543c3b75f69318ebccd2363eff780c4e9
Alec Smecher <alec@smecher.bc.ca>
2022-11-22 19:34:22 -0800

    pkp/pkp-lib#7191 Adapt content creation tests for new submission wizard

--- cypress/tests/data/60-content/MsmithSubmission.spec.js
+++ cypress/tests/data/60-content/MsmithSubmission.spec.js
@@ -11,6 +11,153 @@
  */
 
 describe('Data suite tests', function() {
+
+	let submission;
+	before(function() {
+		const title = 'Open Development: Networked Innovations in International Development';
+		submission = {
+			id: 0,
+			prefix: '',
+			title: title,
+			subtitle: '',
+			'type': 'editedVolume',
+			'abstract': 'The emergence of open networked models made possible by digital technology has the potential to transform international development. Open network structures allow people to come together to share information, organize, and collaborate. Open development harnesses this power to create new organizational forms and improve people’s lives; it is not only an agenda for research and practice but also a statement about how to approach international development. In this volume, experts explore a variety of applications of openness, addressing challenges as well as opportunities.',
+			'keywords': [
+				'International Development',
+				'ICT'
+			],
+			'submitterRole': 'Volume editor',
+			'additionalAuthors': [
+				{
+					'givenName': {en_US: 'Yochai'},
+					'familyName': {en_US: 'Benkler'},
+					'country': 'US',
+					// 'affiliation': '',
+					'email': 'ybenkler@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				},
+				{
+					'givenName': {en_US: 'Katherine'},
+					'familyName': {en_US: 'Reilly'},
+					'country': 'CA',
+					// 'affiliation': '',
+					'email': 'kreilly@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				},
+				{
+					'givenName': {en_US: 'Melissa'},
+					'familyName': {en_US: 'Loudon'},
+					'country': 'US',
+					// 'affiliation': '',
+					'email': 'mloudon@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				},
+				{
+					'givenName': {en_US: 'Ulrike'},
+					'familyName': {en_US: 'Rivett'},
+					'country': 'SA',
+					// 'affiliation': '',
+					'email': 'urivett@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				},
+				{
+					'givenName': {en_US: 'Mark'},
+					'familyName': {en_US: 'Graham'},
+					'country': 'GB',
+					// 'affiliation': '',
+					'email': 'mgraham@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				},
+				{
+					'givenName': {en_US: 'Håvard'},
+					'familyName': {en_US: 'Haarstad'},
+					'country': 'NO',
+					// 'affiliation': '',
+					'email': 'hhaarstad@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				},
+				{
+					'givenName': {en_US: 'Marshall'},
+					'familyName': {en_US: 'Smith'},
+					'country': 'US',
+					// 'affiliation': '',
+					'email': 'masmith@mailinator.com',
+					userGroupId: Cypress.env('authorUserGroupId')
+				}
+			],
+			files: [
+				{
+					'file': 'dummy.pdf',
+					'fileName': 'preface.pdf',
+					'mimeType': 'application/pdf',
+					'genre': Cypress.env('defaultGenre')
+				},
+				{
+					'file': 'dummy.pdf',
+					'fileName': 'introduction.pdf',
+					'mimeType': 'application/pdf',
+					'genre': Cypress.env('defaultGenre')
+				},
+				{
+					'file': 'dummy.pdf',
+					'fileName': 'chapter1.pdf',
+					'mimeType': 'application/pdf',
+					'genre': Cypress.env('defaultGenre')
+				},
+				{
+					'file': 'dummy.pdf',
+					'fileName': 'chapter2.pdf',
+					'mimeType': 'application/pdf',
+					'genre': Cypress.env('defaultGenre')
+				},
+				{
+					'file': 'dummy.pdf',
+					'fileName': 'chapter3.pdf',
+					'mimeType': 'application/pdf',
+					'genre': Cypress.env('defaultGenre')
+				},
+				{
+					'file': 'dummy.pdf',
+					'fileName': 'chapter4.pdf',
+					'mimeType': 'application/pdf',
+					'genre': Cypress.env('defaultGenre')
+				},
+			],
+			'chapters': [
+				{
+					'title': 'Preface',
+					'contributors': ['Yochai Benkler'],
+					files: ['preface.pdf']
+				},
+				{
+					'title': 'Introduction',
+					'contributors': ['Matthew Smith', 'Katherine Reilly'],
+					files: ['introduction.pdf']
+				},
+				{
+					'title': 'The Emergence of Open Development in a Network Society',
+					'contributors': ['Matthew Smith', 'Katherine Reilly'],
+					files: ['chapter1.pdf']
+				},
+				{
+					'title': 'Enacting Openness in ICT4D Research',
+					'contributors': ['Melissa Loudon', 'Ulrike Rivett'],
+					files: ['chapter2.pdf']
+				},
+				{
+					'title': 'Transparency and Development: Ethical Consumption through Web 2.0 and the Internet of Things',
+					'contributors': ['Mark Graham', 'Håvard Haarstad'],
+					files: ['chapter3.pdf']
+				},
+				{
+					'title': 'Open Educational Resources: Opportunities and Challenges for the Developing World',
+					'contributors': ['Marshall Smith'],
+					files: ['chapter4.pdf']
+				}
+			]
+		}
+	});
+
 	it('Create a submission', function() {
 		cy.register({
 			'username': 'msmith',
@@ -20,99 +167,19 @@ describe('Data suite tests', function() {
 			'country': 'Canada'
 		});
 
-		var submission = {
-			'type': 'editedVolume',
-			'title': 'Open Development: Networked Innovations in International Development',
-			'abstract': 'The emergence of open networked models made possible by digital technology has the potential to transform international development. Open network structures allow people to come together to share information, organize, and collaborate. Open development harnesses this power to create new organizational forms and improve people’s lives; it is not only an agenda for research and practice but also a statement about how to approach international development. In this volume, experts explore a variety of applications of openness, addressing challenges as well as opportunities.',
-			'keywords': [
-				'International Development',
-				'ICT'
-			],
-			'submitterRole': 'Volume editor',
-			'additionalAuthors': [
-				{
-					'givenName': 'Yochai',
-					'familyName': 'Benkler',
-					'country': 'United States',
-					// 'affiliation': '',
-					'email': 'ybenkler@mailinator.com'
-				},
-				{
-					'givenName': 'Katherine',
-					'familyName': 'Reilly',
-					'country': 'Canada',
-					// 'affiliation': '',
-					'email': 'kreilly@mailinator.com'
-				},
-				{
-					'givenName': 'Melissa',
-					'familyName': 'Loudon',
-					'country': 'United States',
-					// 'affiliation': '',
-					'email': 'mloudon@mailinator.com'
-				},
-				{
-					'givenName': 'Ulrike',
-					'familyName': 'Rivett',
-					'country': 'South Africa',
-					// 'affiliation': '',
-					'email': 'urivett@mailinator.com'
-				},
-				{
-					'givenName': 'Mark',
-					'familyName': 'Graham',
-					'country': 'United Kingdom',
-					// 'affiliation': '',
-					'email': 'mgraham@mailinator.com'
-				},
-				{
-					'givenName': 'Håvard',
-					'familyName': 'Haarstad',
-					'country': 'Norway',
-					// 'affiliation': '',
-					'email': 'hhaarstad@mailinator.com'
-				},
-				{
-					'givenName': 'Marshall',
-					'familyName': 'Smith',
-					'country': 'United States',
-					// 'affiliation': '',
-					'email': 'masmith@mailinator.com'
-				}
-			],
-			'chapters': [
-				{
-					'title': 'Preface',
-					'contributors': ['Yochai Benkler']
-				},
-				{
-					'title': 'Introduction',
-					'contributors': ['Matthew Smith', 'Katherine Reilly']
-				},
-				{
-					'title': 'The Emergence of Open Development in a Network Society',
-					'contributors': ['Matthew Smith', 'Katherine Reilly']
-				},
-				{
-					'title': 'Enacting Openness in ICT4D Research',
-					'contributors': ['Melissa Loudon', 'Ulrike Rivett']
-				},
-				{
-					'title': 'Transparency and Development: Ethical Consumption through Web 2.0 and the Internet of Things',
-					'contributors': ['Mark Graham', 'Håvard Haarstad']
-				},
-				{
-					'title': 'Open Educational Resources: Opportunities and Challenges for the Developing World',
-					'contributors': ['Marshall Smith']
-				}
-			]
-		};
-		cy.createSubmission(submission);
+		cy.getCsrfToken();
+		cy.window()
+			.then(() => {
+				return cy.createSubmissionWithApi(submission, this.csrfToken);
+			})
+			.then(xhr => {
+				return cy.submitSubmissionWithApi(submission.id, this.csrfToken);
+			});
 		cy.logout();
 
 		cy.findSubmissionAsEditor('dbarnes', null, 'Smith');
 		cy.clickDecision('Send to Internal Review');
-		cy.recordDecisionSendToReview('Send to Internal Review', ['Matthew Smith'], submission.chapters.map(chapter => chapter.title.substring(0, 35)));
+		cy.recordDecisionSendToReview('Send to Internal Review', ['Matthew Smith'], submission.files.map(file => file.fileName));
 		cy.isActiveStageTab('Internal Review');
 		cy.assignReviewer('Julie Janssen');
 		cy.assignReviewer('Paul Hudson');
