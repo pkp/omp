f43f99709811e28193fb2a250a86ee63e0017489
Nate Wright <natew@notthisway.com>
2023-02-01 15:40:14 +0000

    pkp/pkp-lib#8562 Fix keywords location in submission wizard review test

--- cypress/tests/data/60-content/AfinkelSubmission.cy.js
+++ cypress/tests/data/60-content/AfinkelSubmission.cy.js
@@ -305,18 +305,18 @@ describe('Data suite tests', function() {
 			.parents('.submissionWizard__reviewPanel')
 			.find('h4').contains('Title').siblings('.submissionWizard__reviewPanel__item__value').contains(submission.title)
 			.parents('.submissionWizard__reviewPanel')
+			.find('h4').contains('Keywords').siblings('.submissionWizard__reviewPanel__item__value').contains('None provided')
+			.parents('.submissionWizard__reviewPanel')
 			.find('h4').contains('Abstract').siblings('.submissionWizard__reviewPanel__item__value').contains(submission.abstract);
 		cy.get('h3').contains('Details (French/Français (Canada))') // FIXME: Should be (French)
 			.parents('.submissionWizard__reviewPanel')
 			.find('h4').contains('Title').siblings('.submissionWizard__reviewPanel__item__value').contains('None provided')
 			.parents('.submissionWizard__reviewPanel')
-			.find('h4').contains('Abstract').siblings('.submissionWizard__reviewPanel__item__value').contains('None provided');
-		cy.get('h3').contains('For the Editors (English/English)') // FIXME: Should be (English)
-			.parents('.submissionWizard__reviewPanel')
 			.find('h4').contains('Keywords').siblings('.submissionWizard__reviewPanel__item__value').contains('None provided')
-		cy.get('h3').contains('For the Editors (French/Français (Canada))') // FIXME: Should be (English)
 			.parents('.submissionWizard__reviewPanel')
-			.find('h4').contains('Keywords').siblings('.submissionWizard__reviewPanel__item__value').contains('None provided');
+			.find('h4').contains('Abstract').siblings('.submissionWizard__reviewPanel__item__value').contains('None provided');
+		cy.get('h3').contains('For the Editors (English/English)'); // FIXME: Should be (English)
+		cy.get('h3').contains('For the Editors (French/Français (Canada))'); // FIXME: Should be (English)
 
 		// Submit
 		cy.contains('Make a Submission: Review');
