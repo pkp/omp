514057811cbd02048c47828b9ddc218ebf7aeb42
franz-dev <72202910+franz-dev@users.noreply.github.com>
2023-03-07 21:07:23 +0100

    #8758 not showing content in tab Identifiers (#1360)
    Edit Chapter not showing content in tab Identifiers #8758

--- controllers/tab/pubIds/form/PublicIdentifiersForm.php
+++ controllers/tab/pubIds/form/PublicIdentifiersForm.php
@@ -29,7 +29,7 @@ class PublicIdentifiersForm extends PKPPublicIdentifiersForm
     public function fetch($request, $template = null, $display = false)
     {
         $templateMgr = TemplateManager::getManager($request);
-        $enablePublisherId = $request->getContext()->getData('enablePublisherId');
+        $enablePublisherId = (array) $request->getContext()->getData('enablePublisherId');
         $templateMgr->assign([
             'enablePublisherId' => (is_a($this->getPubObject(), 'Chapter') && in_array('chapter', $enablePublisherId)) ||
                     (is_a($this->getPubObject(), 'Representation') && in_array('representation', $enablePublisherId)) ||
