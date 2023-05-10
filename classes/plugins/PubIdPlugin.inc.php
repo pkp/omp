a6305abd7613d044e317299b40a14929b9a0055d
Bozana Bokan <bozana.bokan@posteo.net>
2023-05-10 14:13:39 +0200

    pkp/pkp-lib#8940 consider urnCheckNo setting, fix checkDuplicate, fix publish preview, clear old code

--- classes/plugins/PubIdPlugin.inc.php
+++ classes/plugins/PubIdPlugin.inc.php
@@ -52,27 +52,24 @@ abstract class PubIdPlugin extends PKPPubIdPlugin {
 		$pubObjectType = $this->getPubObjectType($pubObject);
 
 		// Initialize variables for publication objects.
-		$submission = ($pubObjectType == 'Submission' ? $pubObject : null);
+		$submission = null;
+		// Publication is actually handled differently now, but keep it here however for now.
+		$publication = ($pubObjectType == 'Publication' ? $pubObject : null);
 		$representation = ($pubObjectType == 'Representation' ? $pubObject : null);
 		$submissionFile = ($pubObjectType == 'SubmissionFile' ? $pubObject : null);
 		$chapter = ($pubObjectType == 'Chapter' ? $pubObject : null);
 
-		// Get the context id.
-		if ($pubObjectType == 'Submission') {
-			$contextId = $pubObject->getContextId();
-		} else {
-			// Retrieve the submission.
-			if (is_a($pubObject, 'Chapter') || is_a($pubObject, 'Representation')) {
-				$publication = Services::get('publication')->get($pubObject->getData('publicationId'));
-				$submission = Services::get('submission')->get($publication->getData('submissionId'));
-			} else {
-				assert(is_a($pubObject, 'SubmissionFile'));
-				$submission = Services::get('submission')->get($pubObject->getData('submissionId'));
-			}
-			if (!$submission) return null;
-			// Now we can identify the context.
-			$contextId = $submission->getContextId();
+		// Retrieve the submission.
+		if (is_a($pubObject, 'Chapter') || is_a($pubObject, 'Representation')) {
+			$publication = Services::get('publication')->get($pubObject->getData('publicationId'));
+			$submission = Services::get('submission')->get($publication->getData('submissionId'));
+		} else { // Publication or SubmissionFile
+			$submission = Services::get('submission')->get($pubObject->getData('submissionId'));
 		}
+		if (!$submission) return null;
+		// Now we can identify the context.
+		$contextId = $submission->getContextId();
+
 		// Check the context
 		$context = $this->getContext($contextId);
 		if (!$context) return null;
@@ -159,7 +156,7 @@ abstract class PubIdPlugin extends PKPPubIdPlugin {
 	 * @copydoc PKPPubIdPlugin::getDAOs()
 	 */
 	function getDAOs() {
-		return array_merge(parent::getDAOs(), array('Chapter' => DAORegistry::getDAO('ChapterDAO')));
+		return array_merge(parent::getDAOs(), array(DAORegistry::getDAO('ChapterDAO')));
 	}
 
 	/**
@@ -175,7 +172,7 @@ abstract class PubIdPlugin extends PKPPubIdPlugin {
 			}
 		}
 
-		return true;
+		return parent::checkDuplicate($pubId, $pubObjectType, $excludeId, $contextId);
 	}
 }
 
