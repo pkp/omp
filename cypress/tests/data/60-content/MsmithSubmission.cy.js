d62deacc82ae38c9b843a3ed8890ccb19908ab71
Jonas Raoni Soares da Silva <github@raoni.org>
2023-05-13 08:49:13 +0300

    pkp/pkp-lib#8951 Fixed path on header

--- cypress/tests/data/60-content/MsmithSubmission.cy.js
+++ cypress/tests/data/60-content/MsmithSubmission.cy.js
@@ -1,5 +1,5 @@
 /**
- * @file cypress/tests/data/60-content/MsmithSubmission.spec.js
+ * @file cypress/tests/data/60-content/MsmithSubmission.cy.js
  *
  * Copyright (c) 2014-2021 Simon Fraser University
  * Copyright (c) 2000-2021 John Willinsky
