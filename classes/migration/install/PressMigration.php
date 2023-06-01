c135fde069862f3b44c63c73498dfc6656275ed5
Jonas Raoni Soares da Silva <github@raoni.org>
2023-06-01 14:21:12 +0300

    pkp/pkp-lib#9040 Removed references to press_settings.setting_type

--- classes/migration/install/PressMigration.php
+++ classes/migration/install/PressMigration.php
@@ -47,7 +47,6 @@ class PressMigration extends \PKP\migration\Migration
             $table->string('locale', 14)->default('');
             $table->string('setting_name', 255);
             $table->text('setting_value')->nullable();
-            $table->string('setting_type', 6)->nullable();
 
             $table->unique(['press_id', 'locale', 'setting_name'], 'press_settings_unique');
         });
