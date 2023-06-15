d8d3229f56d61352eaed65afd17c969e32fd0035
Alec Smecher <alec@smecher.bc.ca>
2023-06-15 15:20:06 -0700

    https://gitlab.erudit.org/coalitionpublica/support/-/issues/125 Add warning about changing repository ID

--- config.TEMPLATE.inc.php
+++ config.TEMPLATE.inc.php
@@ -423,7 +423,8 @@ result_cache_hours = 1
 ; Enable OAI front-end to the site
 oai = On
 
-; OAI Repository identifier
+; OAI Repository identifier. This setting forms part of OAI-PMH record IDs.
+; Changing this setting may affect existing clients and is not recommended.
 repository_id = omp.pkp.sfu.ca
 
 
