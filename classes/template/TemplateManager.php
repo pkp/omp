e64e04603232c3ea062cd8a5e341347266ed22c9
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-05-06 22:43:54 +0800

    pkp/pkp-lib#9890 Add icon to be used for Catalog link

--- classes/template/TemplateManager.php
+++ classes/template/TemplateManager.php
@@ -135,6 +135,7 @@ class TemplateManager extends PKPTemplateManager
                 'name' => __('navigation.catalog'),
                 'url' => $router->url($request, null, 'manageCatalog'),
                 'isCurrent' => $request->getRequestedPage() === 'manageCatalog',
+                'icon' => 'Catalog'
             ];
 
             $index = array_search('submissions', array_keys($menu));
