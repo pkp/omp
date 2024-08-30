6dbefefb5f36f058f60b6f4fd379a551b00bb4e0
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-08-31 01:29:50 +0800

    pkp/pkp-lib#9744 Update import of JobsPage, FailedJobsPage and FailedJobDetailsPage components

--- js/load.js
+++ js/load.js
@@ -33,9 +33,9 @@ import StatsContextPage from '@/components/Container/StatsContextPage.vue';
 import StatsUsersPage from '@/components/Container/StatsUsersPage.vue';
 import SubmissionWizardPage from '@/components/Container/SubmissionWizardPageOMP.vue';
 import WorkflowPage from '@/components/Container/WorkflowPageOMP.vue';
-import JobsPage from '@/components/Container/JobsPage.vue';
-import FailedJobsPage from '@/components/Container/FailedJobsPage.vue';
-import FailedJobDetailsPage from '@/components/Container/FailedJobDetailsPage.vue';
+import JobsPage from '@/pages/jobs/JobsPage.vue';
+import FailedJobsPage from '@/pages/jobs/FailedJobsPage.vue';
+import FailedJobDetailsPage from '@/pages/jobs/FailedJobDetailsPage.vue';
 import ManageCatalogPage from '@/components/Container/ManageCatalogPage.vue';
 
 // Expose Vue, the registry and controllers in a global var
