185f0fe550ece0f20dd499b550dcdf71a5d8140b
Touhidur Rahman <abir.ruet@gmail.com>
2024-09-22 16:25:14 +0600

    pkp/pkp-lib#9899 fixed PHPUnit deprecation warning

--- tests/jobs/statistics/ProcessUsageStatsLogFileTest.php
+++ tests/jobs/statistics/ProcessUsageStatsLogFileTest.php
@@ -37,7 +37,7 @@ class ProcessUsageStatsLogFileTest extends PKPTestCase
     /**
      * Content example from OJS 3.4.0
      */
-    protected $dummyFileContent = '{"time":"2023-08-07 17:27:11","ip":"228dc4e5b6424e9dad52f21261cb2ab5f4651d9cb426d6fdb3d71d5ab8e2ae83","userAgent":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko\/20100101 Firefox\/115.0","canonicalUrl":"http:\/\/ojs-stable-3_4_0.test\/index.php\/publicknowledge\/index","assocType":256,"contextId":1,"submissionId":null,"representationId":null,"submissionFileId":null,"fileType":null,"country":null,"region":null,"city":null,"institutionIds":[],"version":"3.4.0.0","issueId":null,"issueGalleyId":null}';
+    protected $dummyFileContent = '{"time":"2023-08-07 17:27:11","ip":"228dc4e5b6424e9dad52f21261cb2ab5f4651d9cb426d6fdb3d71d5ab8e2ae83","userAgent":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko\/20100101 Firefox\/115.0","canonicalUrl":"http:\/\/ojs-stable-3_4_0.test\/index.php\/publicknowledge\/index","assocType":512,"contextId":1,"submissionId":null,"representationId":null,"submissionFileId":null,"fileType":null,"country":null,"region":null,"city":null,"institutionIds":[],"version":"3.4.0.0","issueId":null,"issueGalleyId":null}';
 
     /**
      * Test job is a proper instance
