7e563d6acc41f762cbe6d9fedf98e35666fd6f4d
Kaitlin Newson <kaitlin.newson@gmail.com>
2024-10-02 09:47:46 -0300

    pkp/pkp-lib#10046 fix onix export calls to get localized data

--- classes/publication/Publication.php
+++ classes/publication/Publication.php
@@ -3,8 +3,8 @@
 /**
  * @file classes/publication/Publication.php
  *
- * Copyright (c) 2016-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2016-2024 Simon Fraser University
+ * Copyright (c) 2003-2024 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class Publication
@@ -43,6 +43,31 @@ class Publication extends PKPPublication
         // Spaces are stripped from the locale strings, so we have to add the
         // space in here.
         return join(__('common.commaListSeparator') . ' ', $editorNames);
+    }
+
+    /**
+     * Get the URL to a cover image
+     *
+     * @param int $contextId
+     * @param string $preferredLocale Return the cover image in a specified locale.
+     *
+     * @return string
+     */
+    public function getCoverImageUrl($contextId, $preferredLocale = null)
+    {
+        $coverImage = $this->getData('coverImage', $preferredLocale);
+
+        if (!$coverImage) {
+            return Application::get()->getRequest()->getBaseUrl() . '/templates/images/book-default.png';
+        }
+
+        $publicFileManager = new PublicFileManager();
+
+        return join('/', [
+            Application::get()->getRequest()->getBaseUrl(),
+            $publicFileManager->getContextFilesPath($contextId),
+            $coverImage['uploadName'],
+        ]);
     }
 
     /**
