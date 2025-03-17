a2b43b4fe87e56027cbf9b4865e19cb1b44a31cf
Alec Smecher <alec@smecher.bc.ca>
2025-03-17 14:16:23 -0700

    pkp/pkp-lib#10766 DashboardHandlerNext to DashboardHandler

--- pages/workflow/WorkflowHandler.php
+++ pages/workflow/WorkflowHandler.php
@@ -19,18 +19,6 @@ namespace APP\pages\workflow;
 use APP\components\listPanels\ContributorsListPanel;
 use APP\core\Application;
 use APP\core\Request;
-use APP\decision\types\AcceptFromInternal;
-use APP\decision\types\CancelInternalReviewRound;
-use APP\decision\types\DeclineInternal;
-use APP\decision\types\RecommendAcceptInternal;
-use APP\decision\types\RecommendDeclineInternal;
-use APP\decision\types\RecommendRevisionsInternal;
-use APP\decision\types\RecommendSendExternalReview;
-use APP\decision\types\RequestRevisionsInternal;
-use APP\decision\types\RevertDeclineInternal;
-use APP\decision\types\SendExternalReview;
-use APP\decision\types\SendInternalReview;
-use APP\decision\types\SkipInternalReview;
 use APP\facades\Repo;
 use APP\file\PublicFileManager;
 use APP\publication\Publication;
@@ -39,23 +27,8 @@ use APP\template\TemplateManager;
 use PKP\components\forms\publication\TitleAbstractForm;
 use PKP\context\Context;
 use PKP\core\PKPApplication;
-use PKP\decision\types\Accept;
-use PKP\decision\types\BackFromCopyediting;
-use PKP\decision\types\BackFromProduction;
-use PKP\decision\types\CancelReviewRound;
-use PKP\decision\types\Decline;
-use PKP\decision\types\InitialDecline;
-use PKP\decision\types\RecommendAccept;
-use PKP\decision\types\RecommendDecline;
-use PKP\decision\types\RecommendRevisions;
-use PKP\decision\types\RequestRevisions;
-use PKP\decision\types\RevertDecline;
-use PKP\decision\types\RevertInitialDecline;
-use PKP\decision\types\SendToProduction;
-use PKP\decision\types\SkipExternalReview;
 use PKP\notification\Notification;
 use PKP\pages\workflow\PKPWorkflowHandler;
-use PKP\plugins\Hook;
 use PKP\security\Role;
 
 class WorkflowHandler extends PKPWorkflowHandler
@@ -71,13 +44,10 @@ class WorkflowHandler extends PKPWorkflowHandler
             [Role::ROLE_ID_SUB_EDITOR, Role::ROLE_ID_MANAGER, Role::ROLE_ID_SITE_ADMIN, Role::ROLE_ID_ASSISTANT],
             [
                 'access', 'index', 'submission',
-                'editorDecisionActions', // Submission & review
                 'internalReview', // Internal review
                 'externalReview', // External review
                 'editorial',
                 'production',
-                'submissionHeader',
-                'submissionProgressBar',
             ]
         );
     }
@@ -206,142 +176,6 @@ class WorkflowHandler extends PKPWorkflowHandler
                 return Notification::NOTIFICATION_TYPE_EDITOR_ASSIGNMENT_PRODUCTION;
         }
         return null;
-    }
-
-    protected function _getRepresentationsGridUrl($request, $submission)
-    {
-        return $request->getDispatcher()->url(
-            $request,
-            PKPApplication::ROUTE_COMPONENT,
-            null,
-            'grid.catalogEntry.PublicationFormatGridHandler',
-            'fetchGrid',
-            null,
-            [
-                'submissionId' => $submission->getId(),
-                'publicationId' => '__publicationId__',
-            ]
-        );
-    }
-
-    protected function getStageDecisionTypes(int $stageId): array
-    {
-        $submission = $this->getAuthorizedContextObject(Application::ASSOC_TYPE_SUBMISSION);
-        $request = Application::get()->getRequest();
-        $reviewRoundId = (int) $request->getUserVar('reviewRoundId');
-
-        switch ($stageId) {
-            case WORKFLOW_STAGE_ID_SUBMISSION:
-                $decisionTypes = [
-                    new SkipInternalReview(),
-                    new SkipExternalReview(),
-                ];
-                if ($submission->getData('status') === Submission::STATUS_DECLINED) {
-                    $decisionTypes[] = new RevertInitialDecline();
-                } elseif ($submission->getData('status') === Submission::STATUS_QUEUED) {
-                    $decisionTypes[] = new InitialDecline();
-                }
-                $decisionTypes[] = new SendInternalReview();
-                break;
-            case WORKFLOW_STAGE_ID_INTERNAL_REVIEW:
-                $decisionTypes = [
-                    new RequestRevisionsInternal(),
-                    new SendExternalReview(),
-                    new AcceptFromInternal(),
-                ];
-                $cancelInternalReviewRound = new CancelInternalReviewRound();
-                if ($cancelInternalReviewRound->canRetract($submission, $reviewRoundId)) {
-                    $decisionTypes[] = $cancelInternalReviewRound;
-                }
-                if ($submission->getData('status') === Submission::STATUS_DECLINED) {
-                    $decisionTypes[] = new RevertDeclineInternal();
-                } elseif ($submission->getData('status') === Submission::STATUS_QUEUED) {
-                    $decisionTypes[] = new DeclineInternal();
-                }
-                break;
-            case WORKFLOW_STAGE_ID_EXTERNAL_REVIEW:
-                $decisionTypes = [
-                    new RequestRevisions(),
-                    new Accept(),
-                ];
-                $cancelReviewRound = new CancelReviewRound();
-                if ($cancelReviewRound->canRetract($submission, $reviewRoundId)) {
-                    $decisionTypes[] = $cancelReviewRound;
-                }
-                if ($submission->getData('status') === Submission::STATUS_DECLINED) {
-                    $decisionTypes[] = new RevertDecline();
-                } elseif ($submission->getData('status') === Submission::STATUS_QUEUED) {
-                    $decisionTypes[] = new Decline();
-                }
-                break;
-            case WORKFLOW_STAGE_ID_EDITING:
-                $decisionTypes = [
-                    new SendToProduction(),
-                    new BackFromCopyediting(),
-                ];
-                break;
-            case WORKFLOW_STAGE_ID_PRODUCTION:
-                $decisionTypes = [
-                    new BackFromProduction(),
-                ];
-                break;
-        }
-
-        Hook::call('Workflow::Decisions', [&$decisionTypes, $stageId]);
-
-        return $decisionTypes;
-    }
-
-    protected function getStageRecommendationTypes(int $stageId): array
-    {
-        switch ($stageId) {
-            case WORKFLOW_STAGE_ID_INTERNAL_REVIEW:
-                $decisionTypes = [
-                    new RecommendRevisionsInternal(),
-                    new RecommendAcceptInternal(),
-                    new RecommendDeclineInternal(),
-                    new RecommendSendExternalReview(),
-                ];
-                break;
-            case WORKFLOW_STAGE_ID_EXTERNAL_REVIEW:
-                $decisionTypes = [
-                    new RecommendRevisions(),
-                    new RecommendAccept(),
-                    new RecommendDecline(),
-                ];
-                break;
-            default:
-                $decisionTypes = [];
-        }
-
-
-        Hook::call('Workflow::Recommendations', [$decisionTypes, $stageId]);
-
-        return $decisionTypes;
-    }
-
-    protected function getPrimaryDecisionTypes(): array
-    {
-        return [
-            SkipInternalReview::class,
-            SendExternalReview::class,
-            AcceptFromInternal::class,
-            Accept::class,
-            SendToProduction::class,
-        ];
-    }
-
-    protected function getWarnableDecisionTypes(): array
-    {
-        return [
-            InitialDecline::class,
-            DeclineInternal::class,
-            Decline::class,
-            CancelInternalReviewRound::class,
-            CancelReviewRound::class,
-            BackFromCopyediting::class,
-            BackFromProduction::class,
-        ];
     }
 
     protected function getTitleAbstractForm(string $latestPublicationApiUrl, array $locales, Publication $latestPublication, Context $context): TitleAbstractForm
