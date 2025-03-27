567c0eed0c36e39f4c21cbe7146b803718a6a6ce
taslangraham <gtaslan1@gmail.com>
2025-03-27 17:46:54 -0500

    pkp/pkp-lib#11125 Remove locales duplicated at app level

--- classes/migration/upgrade/v3_4_0/I7264_UpdateEmailTemplates.php
+++ classes/migration/upgrade/v3_4_0/I7264_UpdateEmailTemplates.php
@@ -26,8 +26,6 @@ class I7264_UpdateEmailTemplates extends \PKP\migration\upgrade\v3_4_0\I7264_Upd
                 $newVariable = 'pressUrl';
             } elseif ($newVariable === 'contextSignature') {
                 $newVariable = 'pressSignature';
-            } elseif ($newVariable === 'contextAcronym') {
-                $newVariable = 'pressAcronym';
             }
         });
 
--- classes/migration/upgrade/v3_5_0/I11125_UpdateEmailTemplateVariables.php
+++ classes/migration/upgrade/v3_5_0/I11125_UpdateEmailTemplateVariables.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * @file classes/migration/upgrade/v3_4_0/I11125_UpdateEmailTemplateVariables.php
+ * @file classes/migration/upgrade/v3_5_0/I11125_UpdateEmailTemplateVariables.php
  *
- * Copyright (c) 2014-2025 Simon Fraser University
- * Copyright (c) 2000-2025 John Willinsky
+ * Copyright (c) 2025 Simon Fraser University
+ * Copyright (c) 2025 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class I11125_UpdateEmailTemplateVariables
@@ -12,9 +12,9 @@
  * @brief Migration to update Email Template variable names
  */
 
-namespace APP\migration\upgrade\v3_4_0;
+namespace APP\migration\upgrade\v3_5_0;
 
-class I11125_UpdateEmailTemplateVariables extends \PKP\migration\upgrade\v3_4_0\I11125_UpdateEmailTemplateVariables
+class I11125_UpdateEmailTemplateVariables extends \PKP\migration\upgrade\v3_5_0\I11125_UpdateEmailTemplateVariables
 {
     public function oldToNewVariablesMap(): array
     {
