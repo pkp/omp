fd018133d2dbec4027679651c569e3eee79fba73
Kaitlin Newson <kaitlin.newson@gmail.com>
2025-07-04 12:39:09 -0300

    pkp/pkp-lib#9079 cleanup reference to GPL v2

--- cypress/tests/integration/CatalogSearch.cy.js
+++ cypress/tests/integration/CatalogSearch.cy.js
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2016-2021 Simon Fraser University
  * Copyright (c) 2000-2021 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @brief Test for catalog search
  */
