a1a2a981d7aa38bc7c4e9e310d04ed42c976df67
Vitaliy-1 <vitaliybezsh@gmail.com>
2025-08-29 11:53:45 +0300

    pkp/pkp-lib#11643 Fix submission schema data extraction

--- classes/submission/maps/Schema.php
+++ classes/submission/maps/Schema.php
@@ -80,10 +80,10 @@ class Schema extends \PKP\submission\maps\Schema
         foreach ($props as $prop) {
             switch ($prop) {
                 case 'recommendationsIn':
-                    $output[$prop] = $currentReviewRound ? $this->areRecommendationsIn($currentReviewRound, $this->stageAssignments) : null;
+                    $output[$prop] = $currentReviewRound && $this->stageAssignments ? $this->areRecommendationsIn($currentReviewRound, $this->stageAssignments) : null;
                     break;
                 case 'reviewersNotAssigned':
-                    $output[$prop] = $currentReviewRound && $this->reviewAssignments->count() >= intval($this->context->getData('numReviewersPerSubmission'));
+                    $output[$prop] = $currentReviewRound && $this->reviewAssignments && $this->reviewAssignments->count() >= intval($this->context->getData('numReviewersPerSubmission'));
                     break;
                 case 'reviewRounds':
                     $output[$prop] = $this->getPropertyReviewRounds($reviewRounds->flatten());
