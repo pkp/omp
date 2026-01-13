fe4981cf36386788ca6a6fc7beaa83b437fff71e
Alec Smecher <alec@smecher.bc.ca>
2026-01-12 17:09:46 -0800

    pkp/pkp-lib#8920 Convert catalog handler to search toolset

---
+++ classes/search/SubmissionSearchResult.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file classes/search/SubmissionSearchResult.php
+ *
+ * Copyright (c) 2025 Simon Fraser University
+ * Copyright (c) 2025 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @brief A submission search result.
+ */
+
+namespace APP\search;
+
+class SubmissionSearchResult extends \PKP\search\SubmissionSearchResult
+{
+}
