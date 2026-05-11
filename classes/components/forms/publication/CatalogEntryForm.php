606f36a075fe174b3988d8700bdc6cee28a5b3c2
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-05-11 01:44:06 -0400

    pkp/pkp-lib#12584 Add Summary of Changes and Update Type to catalog entry form and group fields

--- classes/components/forms/publication/CatalogEntryForm.php
+++ classes/components/forms/publication/CatalogEntryForm.php
@@ -21,14 +21,23 @@ use APP\facades\Repo;
 use APP\publication\Publication;
 use APP\submission\Submission;
 use PKP\components\forms\FieldAutosuggestPreset;
+use PKP\components\forms\FieldRichTextarea;
 use PKP\components\forms\FieldSelect;
 use PKP\components\forms\FieldText;
 use PKP\components\forms\FieldUploadImage;
 use PKP\components\forms\FormComponent;
+use PKP\publication\enums\UpdateType;
 
 class CatalogEntryForm extends FormComponent
 {
     public const FORM_CATALOG_ENTRY = 'catalogEntry';
+
+    public const GROUP_PLACEMENT = 'placement';
+    public const GROUP_PUBLICATION_TIMING = 'publicationTiming';
+    public const GROUP_VERSION_AND_UPDATES = 'versionAndUpdates';
+    public const GROUP_DISPLAY = 'display';
+    public const GROUP_ACCESS = 'access';
+
     public $id = self::FORM_CATALOG_ENTRY;
     public $method = 'PUT';
 
@@ -51,6 +60,13 @@ class CatalogEntryForm extends FormComponent
         $this->successMessage = __('publication.catalogEntry.success');
         $this->locales = $locales;
 
+        $this
+            ->addGroup(['id' => self::GROUP_PLACEMENT, 'label' => __('publication.placement')])
+            ->addGroup(['id' => self::GROUP_PUBLICATION_TIMING, 'label' => __('publication.publicationTiming')])
+            ->addGroup(['id' => self::GROUP_VERSION_AND_UPDATES, 'label' => __('publication.versionAndUpdates')])
+            ->addGroup(['id' => self::GROUP_DISPLAY, 'label' => __('publication.display')])
+            ->addGroup(['id' => self::GROUP_ACCESS, 'label' => __('publication.access')]);
+
         // Series options
         $seriesOptions = [['value' => '', 'label' => '']];
         $result = Repo::section()
@@ -64,16 +80,15 @@ class CatalogEntryForm extends FormComponent
             ];
         }
 
-        $this->addField(new FieldText('datePublished', [
-            'label' => __('publication.datePublished'),
-            'value' => $publication->getData('datePublished'),
-        ]))
+        $this
             ->addField(new FieldSelect('seriesId', [
+                'groupId' => self::GROUP_PLACEMENT,
                 'label' => __('series.series'),
                 'value' => $publication->getData('seriesId'),
                 'options' => $seriesOptions,
             ]))
             ->addField(new FieldText('seriesPosition', [
+                'groupId' => self::GROUP_PLACEMENT,
                 'label' => __('submission.submit.seriesPosition'),
                 'description' => __('submission.submit.seriesPosition.description'),
                 'value' => $publication->getData('seriesPosition'),
@@ -99,6 +114,7 @@ class CatalogEntryForm extends FormComponent
             $vocabulary = Repo::category()->getCategoryVocabularyStructure($categories);
 
             $this->addField(new FieldAutosuggestPreset('categoryIds', [
+                'groupId' => self::GROUP_PLACEMENT,
                 'label' => __('submission.submit.placement.categories'),
                 'value' => (array) $publication->getData('categoryIds'),
                 'description' => $hasAllBreadcrumbs ? '' : __('submission.categories.circularReferenceWarning'),
@@ -113,12 +129,32 @@ class CatalogEntryForm extends FormComponent
             ]));
         }
 
-        $this->addField(new FieldText('urlPath', [
-            'label' => __('publication.urlPath'),
-            'description' => __('publication.urlPath.description'),
-            'value' => $publication->getData('urlPath'),
-        ]))
+        $this
+            ->addField(new FieldText('datePublished', [
+                'groupId' => self::GROUP_PUBLICATION_TIMING,
+                'label' => __('publication.datePublished'),
+                'value' => $publication->getData('datePublished'),
+            ]))
+            ->addField(new FieldSelect('updateType', [
+                'groupId' => self::GROUP_VERSION_AND_UPDATES,
+                'label' => __('publication.updateType.label'),
+                'description' => __('publication.updateType.description'),
+                'options' => array_map(
+                    fn (UpdateType $case) => ['value' => $case->value, 'label' => $case->label()],
+                    UpdateType::cases()
+                ),
+                'value' => $publication->getData('updateType') ?? UpdateType::NEW_VERSION->value,
+                'size' => 'large',
+            ]))
+            ->addField(new FieldRichTextarea('summaryOfChanges', [
+                'groupId' => self::GROUP_VERSION_AND_UPDATES,
+                'label' => __('submission.form.summaryOfChanges'),
+                'description' => __('publication.summaryOfChanges.description'),
+                'isMultilingual' => true,
+                'value' => $publication->getData('summaryOfChanges'),
+            ]))
             ->addField(new FieldUploadImage('coverImage', [
+                'groupId' => self::GROUP_DISPLAY,
                 'label' => __('monograph.coverImage'),
                 'value' => $publication->getData('coverImage'),
                 'isMultilingual' => true,
@@ -126,6 +162,12 @@ class CatalogEntryForm extends FormComponent
                 'options' => [
                     'url' => $temporaryFileApiUrl,
                 ],
+            ]))
+            ->addField(new FieldText('urlPath', [
+                'groupId' => self::GROUP_ACCESS,
+                'label' => __('publication.urlPath'),
+                'description' => __('publication.urlPath.description'),
+                'value' => $publication->getData('urlPath'),
             ]));
     }
 }
